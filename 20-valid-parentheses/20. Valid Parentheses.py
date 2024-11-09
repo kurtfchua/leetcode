fe92ee791cd5e0e13970e4a4624e536f56af24b8
kurtfchua <94031312+kurtfchua@users.noreply.github.com>
2024-11-08 21:54:40 -0800

    Runtime: 52 ms (9.56%), Memory: 16.4 MB (99.79%) - Gitcode

--- 20-valid-parentheses/20. Valid Parentheses.py
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -1,31 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # iterate through string, if char is an open parenthesis add to stack 
-        # if char is a close parenthesis check if its mapped open parenthesis pair is on top of the stack
-        # if the paired open parenthesis is on top of the stack, pop and continue process
-        # if stack is empty paranthesis is valid
-        if len(s) < 2 or len(s)%2 != 0:
+        if len(s) % 2 != 0:
             return False
 
-        par_dict = {
-            ')': '(', 
-            '}':'{',
-            ']':'['
+        stack = []
+        hashmap = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
         }
-        par_stack = []
 
-        for char in s:
-            if char in par_dict:
-                if par_stack and (par_stack[-1] == par_dict[char]):
-                    par_stack.pop()
+        for c in s:
+            if c in hashmap:
+                if stack and hashmap[c] == stack[-1]:
+                    stack.pop()
                 else:
                     return False
             else:
-                par_stack.append(char)
+                stack.append(c)
 
-        return not par_stack
+        return not stack
+         
 
-    # Time Complexity O(n)
-    # Space Complexity O(n)
-
-        
