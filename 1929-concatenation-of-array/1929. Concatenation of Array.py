5496787504af45abf47b21f2a3e281dc1e607f0c
kurtfchua <94031312+kurtfchua@users.noreply.github.com>
2025-03-23 04:34:48 -0500

    Runtime: 3 ms (15.12%), Memory: 17.8 MB (92.16%) - Gitcode

---
+++ 1929-concatenation-of-array/1929. Concatenation of Array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # Approach 1: return nums + nums
+        # Approach 2: loop through nums twice and append elements to a new array and return it
+        new_array = []
+
+        for i in range(2):
+            for num in nums: 
+                new_array.append(num)
+
+        return new_array
