83e43bb10ffc9e3f852943f7a36006ded5784f5f
kurtfchua <94031312+kurtfchua@users.noreply.github.com>
2025-03-28 17:08:52 -0500

    Runtime: 49 ms (56.56%), Memory: 18.8 MB (50.88%) - Gitcode

--- 707-design-linked-list/707. Design Linked List.py
+++ 707-design-linked-list/707. Design Linked List.py
@@ -8,33 +8,33 @@ class MyLinkedList:
     def __init__(self):
         self.head = Node(0)
         self.size = 0
-    
+
     def get(self, index: int) -> int:
         if index < 0 or index >= self.size:
             return -1
-
-        current = self.head.next   
+        
+        current = self.head.next
         for _ in range(index):
             current = current.next
         
         return current.val
-
+        
     def addAtHead(self, val: int) -> None:
         new_node = Node(val)
         new_node.next = self.head.next
         self.head.next = new_node
-        self.size+=1
+        self.size +=1
 
     def addAtTail(self, val: int) -> None:
-        new_node = Node(val)
         current = self.head
         while current.next:
             current = current.next
+        new_node = Node(val)
         current.next = new_node
-        self.size+=1
+        self.size +=1        
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index < 0 or index > self.size:
+        if index > self.size:
             return
         current = self.head
         for _ in range(index):
@@ -42,18 +42,16 @@ class MyLinkedList:
         new_node = Node(val)
         new_node.next = current.next
         current.next = new_node
-        self.size += 1
-
-    def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >= self.size:
-            return
+        self.size +=1
         
+    def deleteAtIndex(self, index: int) -> None:
+        if index >= self.size:
+            return
         current = self.head
         for _ in range(index):
             current = current.next
         current.next = current.next.next
         self.size -=1
-        
 
 
 # Your MyLinkedList object will be instantiated and called as such:
