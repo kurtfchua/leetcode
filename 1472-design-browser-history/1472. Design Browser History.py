6a346194b3bc1cf163cd6f56ac081157581601ab
kurtfchua <94031312+kurtfchua@users.noreply.github.com>
2025-03-30 01:24:58 -0500

    Runtime: 60 ms (15.73%), Memory: 20.1 MB (64.00%) - Gitcode

--- 1472-design-browser-history/1472. Design Browser History.py
+++ 1472-design-browser-history/1472. Design Browser History.py
@@ -1,38 +1,43 @@
 class PageNode:
-    def __init__(self, val, next=None, prev=None):
+    # node that holds information for our page
+    def __init__(self, val, prev=None, next=None):
         self.val = val
-        self.next = next
         self.prev = prev
+        self.next = None
 
 class BrowserHistory:
-
+    
     def __init__(self, homepage: str):
+        # initially set our current page to homepage
         self.current = PageNode(homepage)
 
     def visit(self, url: str) -> None:
-        # set last node next to new visited node
-        # set visited node prev to current node
-        # set current node to visited node
-        self.current.next = PageNode(url, None, self.current)
-        self.current = self.current.next
-         
+        # create new page with url
+        # assign the current.next page to new page
+        # set new page prev to current
+        # set the current page to new page
+        new_page = PageNode(url, self.current, None)
+        self.current.next = new_page
+        self.current = new_page
+
     def back(self, steps: int) -> str:
-        # decrement back to amount of steps to current node
-        # check if current.prev exists and if steps > 0 in loop
+        # decrement x steps from current page
+        # check if current.prev exists to get to our boundary
+        # return url val
         while self.current.prev and steps > 0:
             self.current = self.current.prev
             steps -= 1
-
         return self.current.val
         
     def forward(self, steps: int) -> str:
-        # increment forward amount of steps to current node
-        # check if current.next exists and if steps > 0 in loop
-        while self.current.next and steps > 0 :
+        # increment x steps from current page
+        # check if current.next exists to get to our boundary
+        # return url val
+        while self.current.next and steps > 0:
             self.current = self.current.next
-            steps -=1 
-        
+            steps -= 1
         return self.current.val
+        
 
 
 # Your BrowserHistory object will be instantiated and called as such:
