aa764a4af4827b5bcd219e02e4786aa9e04c121b
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-07 17:34:17 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (40.68%) - Gitcode

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700. Number of Students Unable to Eat Lunch.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        # get counts of the different preferences in students list
+        # while we iterate through sandwhiches we decrement the counts in dict
+        # add up all the counts after the decrement and return 
+
+        prefs_dict = {}
+        for pref in students:
+            if pref in prefs_dict:
+                prefs_dict[pref] +=1
+            else:
+                prefs_dict[pref] = 1
+        for sandwhich in sandwiches:
+            if prefs_dict.get(sandwhich, 0) > 0:
+                prefs_dict[sandwhich] -= 1
+            else:
+                break
+
+        count = 0
+
+        for pref in prefs_dict:
+            count += prefs_dict[pref]
+
+        return count
+        
