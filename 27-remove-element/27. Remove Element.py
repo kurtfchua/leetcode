188ee1af3a5706a93791d1d02c90efaa763232d6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-09 15:07:10 -0500

    Runtime: 0 ms (100.00%), Memory: 17.6 MB (83.36%) - Gitcode

--- 27-remove-element/27. Remove Element.py
+++ 27-remove-element/27. Remove Element.py
@@ -1,19 +1,20 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # iterate nums using 2 pointers, left and right
-        # right pointer will keep track of elements != val
-        # it will update in the either case 
-        # in the case that elem != val, nums[left] = nums[right]
-        # then we update left accordingly by 1
+        # iterate with 2 pointers i, j 
+        # i will be the next element to be removed
+        # j will be the next element != val
+        # if j != val swap i with j, then increment i and j 
+        # otherwise increment j 
 
-        left = right = 0
+        i = j = 0 
 
-        while right < len(nums):
-            if nums[right] != val:
-                nums[left] = nums[right]
-                left +=1
-                right+=1
+        while j < len(nums):
+            if nums[j] != val: 
+                nums[i] = nums[j]
+                i += 1
+                j += 1
             else:
-                right+=1
+                j+=1
         
-        return left 
+        return i
+        
