6dfc244f78956e51888c6d4e30938a044b3f4c11
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-08 22:32:39 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (8.56%) - Gitcode

---
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def dfs(n):
+            # base case: when we try to get to step 1 or 2
+            # 1 unique way to get to step 1, and 2 unique ways to get to step 2
+            if n == 1 or n == 2: return n 
+            # if result exists in memory just return result
+            if n in memo: return memo[n]
+            # counting all paths that start at step 0 and reach step n
+            # either taking 1 or 2 steps from a current step
+            memo[n] = dfs(n-1) + dfs(n-2)
+            return memo[n]
+        return dfs(n)
+
+        # T O(2^n): we add a new level of 2 new nodes to the recursion tree with each call from a step
+        # S O(n): n is the maximum depth of the tree we travel when collapsing the call stack
