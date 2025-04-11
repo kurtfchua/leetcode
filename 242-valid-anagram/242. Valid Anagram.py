9abb87c63b6856b28eade01a0c99c6604415cc2a
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-10 22:08:20 -0500

    Runtime: 11 ms (72.53%), Memory: 17.9 MB (64.30%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -1,16 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # create 2 dictionaries, where freq of each char in s and t are counted
-        # if dictionaries are equal then they are anagrams 
+        # create a dictionary holding the counts for the first string
+        # now iterate through the second string subtracting the value in the dict by 1
+        # if value is at 0 or value doesn't exist it is not an anagram
+        # otherwise return True
         s_dict = {}
-        t_dict = {}
 
-        for c in s: 
+        for c in s:
             s_dict[c] = s_dict.get(c, 0) + 1
+        
         for c in t:
-            t_dict[c] = t_dict.get(c, 0) + 1
-            
-        return s_dict == t_dict
+            if c not in s_dict or s_dict[c] == 0: 
+                return False
+            s_dict[c] -=1
+        
+        for val in s_dict.values():
+            if val != 0:
+                return False
+                
+        return True
 
 
         
