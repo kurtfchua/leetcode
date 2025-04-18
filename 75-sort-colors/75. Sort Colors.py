867f0ce98db225df2c175a4ae3179d6eacdfbb73
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-17 23:53:32 -0500

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (53.67%) - Gitcode

--- 75-sort-colors/75. Sort Colors.py
+++ 75-sort-colors/75. Sort Colors.py
@@ -3,22 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # bucket sort
-        counts = [0,0,0] # use 0,1,2 as keys when iterating through nums
+        # use bucket sort to sort known values of 0-2
+        counts = [0,0,0]
 
         for num in nums:
             counts[num] += 1
-        
-        # reassign elements in numbs while iterating through counts
 
         i = 0
         for n in range(len(counts)):
             for _ in range(counts[n]):
                 nums[i] = n
                 i += 1
-
+        
         return nums
-
-        # T O(n): We iterate through nums 2 separate times. Once to get counts and once to reassign values from counts.
-        # S O(1): Counts is defined by our range of numbers that we already knew beforehand, it doesn't grow with the list.
-         
+        
