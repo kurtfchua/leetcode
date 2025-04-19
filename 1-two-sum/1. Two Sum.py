d712b22b55fe8ada654383fb87ba7a0ffb2480d8
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-18 19:06:42 -0500

    Runtime: 0 ms (100.00%), Memory: 19.2 MB (6.37%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -1,16 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # iterate through nums, and calculate the diff between target and nums[i]
-        # we will use the diff as the key for the diff of this current index and target and the value as the index it was found in
-        # if a nums[i] is equal to the diff found earlier in the array, return the value of this diff in the dictionary + the current index where we found it
-        diff_map = {}
+        nums_dict = {}
 
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in diff_map:
-                return [i, diff_map[diff]]
+            if nums[i] in nums_dict: 
+                return [nums_dict[nums[i]], i]
             else:
-                diff_map[nums[i]] = i 
+                nums_dict[target-nums[i]] = i 
 
-        # Time Complexity: O(n)
-        # Space Complexity: O(n)
+        return [-1,-1] 
+        
