dc790eeef7bf16a67c250f1cfcbe80c6e96cf65a
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-24 07:26:01 -0500

    Runtime: 3 ms (21.39%), Memory: 18.2 MB (71.67%) - Gitcode

--- 412-fizz-buzz/412. Fizz Buzz.py
+++ 412-fizz-buzz/412. Fizz Buzz.py
@@ -1,8 +1,11 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
+        mem = set()
         result = []
 
         for i in range(1, n+1):
+            if i in mem:
+                result.append(str(i))
             if i % 3 == 0 and i % 5 == 0:
                 result.append("FizzBuzz")
             elif i % 3 == 0:
