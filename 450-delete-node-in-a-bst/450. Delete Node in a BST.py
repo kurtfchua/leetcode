78d307b25c3e2557868a1f8f9b57471214db78e2
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-24 14:55:18 -0500

    Runtime: 0 ms (100.00%), Memory: 21.4 MB (74.98%) - Gitcode

--- 450-delete-node-in-a-bst/450. Delete Node in a BST.py
+++ 450-delete-node-in-a-bst/450. Delete Node in a BST.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def find_min(self,root):
+    def find_min(self, root):
         current = root
         while current and current.left:
             current = current.left
@@ -13,26 +13,32 @@ class Solution:
         return current
 
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if not root:
+        # base-case
+        if not root: 
             return root
         
+        # recursive case
+        # search for node
         if key < root.val:
             root.left = self.deleteNode(root.left, key)
         elif key > root.val:
             root.right = self.deleteNode(root.right, key)
         else:
-            # 0 or 1 child
+            # found node 
+            # target node has 0 or 1 children
             if not root.left:
                 return root.right
             elif not root.right:
                 return root.left
             else:
-                # 2 children
-                min_val = self.find_min(root.right).val # find min in right subtree from root
-                root.val = min_val# assign root value to min
-                root.right = self.deleteNode(root.right, min_val) # remove the min value from right subtree
-        
+                # has 2 children
+                # find minimum value of the right subtree from this root
+                # assign root to minimum value
+                # remove the old minimum value in the right subtree from this root
+                # assign root.right to new right subtree 
+                min = self.find_min(root.right).val
+                root.val = min
+                root.right = self.deleteNode(root.right, min)
+       
         return root
-
-        
         
