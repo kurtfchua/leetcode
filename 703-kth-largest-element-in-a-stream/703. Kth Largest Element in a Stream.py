7beb030ed3b43986887ee946612056a3496ee4a8
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-27 16:23:21 -0500

    Runtime: 115 ms (8.75%), Memory: 23.8 MB (46.93%) - Gitcode

--- 703-kth-largest-element-in-a-stream/703. Kth Largest Element in a Stream.py
+++ 703-kth-largest-element-in-a-stream/703. Kth Largest Element in a Stream.py
@@ -3,44 +3,34 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.heap = []
+
         for num in nums:
             self.push(num)
-            if len(self.heap) > self.k:
-                self.pop()
-        
-    def add(self, val: int) -> int:
-        self.push(val)
-        if len(self.heap) > self.k:
-            self.pop()
-        
-        return self.heap[0]
 
-    
+        while len(self.heap) > self.k:
+            self.pop()
+
     def push(self, val):
         self.heap.append(val)
         i = len(self.heap) - 1
-        
+
         while i > 0 and self.heap[(i-1)//2] > self.heap[i]:
             self.heap[(i-1)//2], self.heap[i] = self.heap[i], self.heap[(i-1)//2]
-            i = (i-1)//2
-
+            i = (i-1) // 2
+    
     def pop(self):
-        if not self.heap:
-            return self.heap
-        if len(self.heap) == 1:
-            return self.heap.pop()
+        if len(self.heap) == 0: return self.heap
+        if len(self.heap) == 1: return self.heap.pop()
 
         res = self.heap[0]
         self.heap[0] = self.heap.pop()
+
         i = 0
 
-        while 2*i+1 < len(self.heap):
-            # check if right child exists
-            # check if right < left
-            # check if right < new node
-            if ((2*i+2) < len(self.heap) and
-            self.heap[2*i+2] < self.heap[2*i+1] and
-            self.heap[2*i+2] < self.heap[i]):
+        while 2*i + 1 < len(self.heap):
+            if (2*i + 2 < len(self.heap) and
+                self.heap[2*i+2] < self.heap[2*i+1] and
+                self.heap[2*i+2] < self.heap[i]):
                 self.heap[2*i+2], self.heap[i] = self.heap[i], self.heap[2*i+2]
                 i = 2*i+2
             elif self.heap[2*i+1] < self.heap[i]:
@@ -48,12 +38,16 @@ class KthLargest:
                 i = 2*i+1
             else:
                 break
-
+        
         return res
 
 
+    def add(self, val: int) -> int:
+        self.push(val)
+        while len(self.heap) > self.k:
+            self.pop()
         
-
+        return self.heap[0]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
