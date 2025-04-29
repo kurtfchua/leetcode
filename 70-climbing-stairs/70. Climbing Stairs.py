163e95700cf1723acd305eb0ca179e2ff85872bd
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-29 15:17:36 -0500

    Runtime: 0 ms (100.00%), Memory: 17.6 MB (95.71%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -11,7 +11,19 @@ class Solution:
             # either taking 1 or 2 steps from a current step
             memo[n] = dfs(n-1) + dfs(n-2)
             return memo[n]
-        return dfs(n)
+        #return dfs(n)
 
+        def dp(n):
+            if n <= 2:
+                return n
+            dp = [1,2]
+            i = 3
+
+            while i <= n:
+                dp[0], dp[1] = dp[1], dp[1] + dp[0]
+                i += 1
+            return dp[1]
+        return dp(n)
+        
         # T O(n): we memo each result after exploring the longest branch. each subproblem is solved once
         # S O(n): n is the maximum depth of the tree we travel when collapsing the call stack
