8e057929686934ecee49e5c0a73c8df2490ab981
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-04-29 17:39:15 -0500

    Runtime: 2 ms (26.56%), Memory: 17.8 MB (46.22%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -2,7 +2,7 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         cache = [[0] * n for _ in range(m)]
 
-        def dfs(r, c, ROWS, COLS, cache):
+        def memo(r, c, ROWS, COLS, cache):
             if r == ROWS or c == COLS:
                 return 0 
             if cache[r][c] > 0:
@@ -10,9 +10,22 @@ class Solution:
             if r == ROWS - 1 and c == COLS - 1:
                 return 1
             
-            cache[r][c] = dfs(r+1, c, ROWS, COLS, cache) + dfs(r, c+1, ROWS, COLS, cache)
+            cache[r][c] = memo(r+1, c, ROWS, COLS, cache) + memo(r, c+1, ROWS, COLS, cache)
             
             return cache[r][c]
+        
+        def dp(rows,cols):
+            prev_row = [0 for _ in range(cols)]
 
-        return dfs(0, 0, m, n, cache)
+            for _ in range(rows-1, -1, -1):
+                current_row = [0 for _ in range(cols)]
+                current_row[cols-1] = 1
+                for c in range(cols-2, -1, -1):
+                    current_row[c] = current_row[c+1] + prev_row[c]
+                prev_row = current_row
+            
+            return prev_row[0]
+        
+        #return memo(0, 0, m, n, cache)
+        return dp(m,n)
         
