b6e63bc81b7a87014c0e820007322b9a1a78e157
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-05 17:11:32 -0500

    Runtime: 0 ms (100.00%), Memory: 19 MB (29.83%) - Gitcode

--- 26-remove-duplicates-from-sorted-array/26. Remove Duplicates from Sorted Array.py
+++ 26-remove-duplicates-from-sorted-array/26. Remove Duplicates from Sorted Array.py
@@ -1,28 +1,15 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # iterate through the array using 2 pointers i and j 
-        # i will hold the next elem that needs to be swapped
-        # j will hold the next unique eleme found
-        # we swap arr[i] and arr[j] when arr[j] != arr[j-1], unique elem found
-        # then increment i and j to perform next swap
-        # otherwise we increment j to find next unique element
-        # i is therefore boundary of the sorted array, return i to get the amount
-       
-        # we start at 1, first elem is already sorted
         i = j = 1
+        
         while j < len(nums):
             if nums[j] != nums[j-1]:
                 nums[i] = nums[j]
-                i +=1
-                j +=1
-            else:
-                j +=1
-        
-        return i 
+                i+=1
+            j+=1
 
-        # T O(n) we have to iterate the array just once 
-        # S O(1) this is done in place, no new data structures were used 
-    
+        return i
 
-
+        # T O(n)
+        # S O(1)
         
