0e48dc00f59afdb140db2427a9eee788d44c4862
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-05 15:11:34 -0500

    Runtime: 8 ms (78.99%), Memory: 17.9 MB (63.89%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -1,20 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict = {}
-        t_dict = {}
-        
-        for i in s:
-            if i in s_dict:
-                s_dict[i] +=1
-            else:
-                s_dict[i] = 1
-        
-        for i in t:
-            if i in t_dict:
-                t_dict[i] +=1
-            else:
-                t_dict[i] = 1
-        
-        return s_dict == t_dict
-        
+        if len(s) != len(t): return False
 
+        s_freq = {}
+
+        for c in s: 
+            s_freq[c] = s_freq.get(c, 0) + 1
+        
+        for c in t: 
+            if c not in s_freq:
+                return False
+            s_freq[c] -= 1
+        
+        for v in s_freq.values():
+            if v != 0:
+                return False
+        
+        return True
+
+        # T O(m+n) - we must iterate through both strings to see if they are anagrams of each other. 
+        # S O(m) - we create a hashmap as large as string s
+        
