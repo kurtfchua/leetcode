0954058be315cfc5d9490edc54f8a8e0e274f414
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-05 17:45:35 -0500

    Runtime: 11 ms (82.95%), Memory: 20.7 MB (62.72%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.py
+++ 49-group-anagrams/49. Group Anagrams.py
@@ -1,12 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = defaultdict(list)
+        # keep a hashmap to store the sorted values of strs as keys
+        # if sorted value is in the hashmap we append the unsorted value
+        # if not we initiate the value with this unsorted string
+        # we then iterate through all the values and append them to a list and return
+        anagrams = defaultdict(list)
 
         for word in strs:
-            sorted_word = sorted(word)
-            groups[''.join(sorted_word)].append(word)
-
-        return list(groups.values())
-
+            sorted_word = ''.join(sorted(word))
+            anagrams[sorted_word].append(word)
+        
+        res = []
+        for v in anagrams.values():
+            res.append(v)
+        
+        return res
 
         
