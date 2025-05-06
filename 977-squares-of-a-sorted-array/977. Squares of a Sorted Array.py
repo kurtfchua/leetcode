53151d50626935c453d0352f4067d3791c98806a
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-06 11:08:55 -0500

    Runtime: 11 ms (52.12%), Memory: 19.6 MB (36.25%) - Gitcode

--- 977-squares-of-a-sorted-array/977. Squares of a Sorted Array.py
+++ 977-squares-of-a-sorted-array/977. Squares of a Sorted Array.py
@@ -1,26 +1,19 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        # have 2 pointers pointing to the start and end of nums list
-        # create a list of length nums consisting only of zeros
-        # compare the 2 locations of the list, whicever is greater we add to the result list
-        # increment and decrement based on which number was chosen
-        result = [0 for x in range(len(nums))]
-        left, right = 0 , len(nums) - 1
-        i = len(result) - 1
+        n = len(nums)
+        res = [0] * n
+        i, j = 0, n - 1
+        for k in range(len(nums)-1,-1,-1):
+            i_squared = nums[i] * nums[i]
+            j_squared = nums[j] * nums[j]
+            if i_squared >= j_squared:
+                res[k] = i_squared
+                i +=1
+            else:
+                res[k] = j_squared
+                j-=1
+        
+        return res
 
-        while left <= right: 
-            left_squared = nums[left] * nums[left]
-            right_squared = nums[right] * nums[right]
-            
-            if left_squared >= right_squared: 
-                result[i] = left_squared
-                left += 1
-            if right_squared > left_squared:
-                result[i] = right_squared
-                right -= 1
-            i -= 1
 
-        return result 
-
-        # T O(n): we must iterate through all of nums to build the new list
-        # S O(n): we create a new list that is the same size as nums
+        
