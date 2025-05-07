e3a26a4a2447b4a415add00a610304854721c667
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-07 00:41:23 -0500

    Runtime: 10 ms (25.27%), Memory: 24.2 MB (13.29%) - Gitcode

--- 347-top-k-frequent-elements/347. Top K Frequent Elements.py
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -1,21 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # get counts for nums
-        # get sorted values list for the hashmap
-        # append to output array the keys whose values match the sorted values
-        # return immediately if len of res is == k 
         hashmap = {}
 
         for num in nums:
             hashmap[num] = hashmap.get(num,0) + 1
 
-        sorted_vals = sorted(list(set(hashmap.values())), reverse=True)
-        res =[]
-        for v in sorted_vals:
-            for num in hashmap:
+        bucket = [[] for _ in range(len(nums)+1)]
+
+        for key, value in hashmap.items():
+            bucket[value].append(key)
+        
+        res = []
+        for i in range(len(bucket)-1, -1, -1):
+            for num in bucket[i]:
+                res.append(num)
                 if len(res) == k:
                     return res
-                if hashmap[num] == v:
-                    res.append(num)
         
         return res
+
+
+        
