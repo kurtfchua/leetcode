48e83004ce8f965fc1a8df91defaf7651d8b2c4a
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-07 04:24:58 -0500

    Runtime: 11 ms (72.43%), Memory: 17.8 MB (86.25%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -1,23 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
+        counts = [0]*26
 
-        s_freq = {}
-
-        for c in s: 
-            s_freq[c] = s_freq.get(c, 0) + 1
+        for c in s:
+            counts[ord(c)-ord('a')]+=1
         
-        for c in t: 
-            if c not in s_freq:
+        for c in t:
+            index = ord(c)-ord('a')
+            if counts[index] < 0:
                 return False
-            s_freq[c] -= 1
+            counts[index] -= 1
         
-        for v in s_freq.values():
-            if v != 0:
-                return False
-        
-        return True
-
-        # T O(m+n) - we must iterate through both strings to see if they are anagrams of each other. 
-        # S O(m) - we create a hashmap as large as string s
+        return not any(counts)
         
