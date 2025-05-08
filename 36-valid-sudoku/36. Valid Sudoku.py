8a5e217257e810794666a15427382ab1985f7e01
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-07 22:30:05 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (5.84%) - Gitcode

--- 36-valid-sudoku/36. Valid Sudoku.py
+++ 36-valid-sudoku/36. Valid Sudoku.py
@@ -1,41 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for row in range(0,9,3):
-            for col in range(0,9,3):
-                seen = set()
-                for i in range(3):
-                    for j in range(3):
-                        if board[row + i][col + j] in seen:
+        # check all rows if unique elements
+        for i in range(9):
+            row_set = set()
+            for j in range(9):
+                if board[i][j] in row_set:
+                    return False
+                if board[i][j] != ".":
+                    row_set.add(board[i][j])
+        
+        # check all cols if unique elements
+        for j in range(9):
+            col_set = set()
+            for i in range(9):
+                if board[i][j] in col_set:
+                    return False
+                if board[i][j] != ".":
+                    col_set.add(board[i][j])
+        
+        # check each 3x3 box, iterate 3x3 box and see if all unique
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                box_set = set()
+                for row in range(3):
+                    for col in range(3):
+                        if board[i+row][j+col] in box_set:
                             return False
-                        if board[row + i][col + j] !=".":
-                            seen.add(board[row + i][col + j])
-
-        return self.check_row(board,9,9) and self.check_col(board,9,9)
+                        if board[i+row][j+col] != ".":
+                            box_set.add(board[i+row][j+col])
         
-        
-    def check_row(self, board, m, n):
-            for i in range(m):
-                row_set = set()
-                for j in range(n):
-                    if board[i][j] in row_set:
-                        return False
-                    if board[i][j] != ".":
-                        row_set.add(board[i][j])
-            return True
-        
-    def check_col(self, board,m,n):
-            for j in range(n):
-                col_set = set()
-                for i in range(m):
-                    if board[i][j] in col_set:
-                        return False
-                    if board[i][j] != ".":
-                        col_set.add(board[i][j])  
-            return True
-
-
-
-            
-
-
-
+        return True
