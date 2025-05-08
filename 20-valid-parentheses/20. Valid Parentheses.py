022473b455a89d08e024139838dc3e1740d5ce28
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-08 02:48:12 -0500

    Runtime: 1 ms (33.03%), Memory: 17.8 MB (50.86%) - Gitcode

--- 20-valid-parentheses/20. Valid Parentheses.py
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -1,33 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # use stack to keep track of the updated 
-        # closing parenthesis when new opening parentheisis is found
-        # when we encounter a closing parenthesis, 
-        # we check if it is equal to the elem on top of stack
-        # otherwise we return False
-        # we use a dictionary to keep track of the pairs
-        # if stack size is 0 all parenthesis are closed properly ahd we return True
-        # otherwise return False
-
+        hashmap = {
+            '(':')',
+            '{':'}',
+            '[':']'
+        }
         stack = []
-        dict = {
-                "(": ")",
-                "[": "]",
-                "{":"}"
-                    }
-        
-        for char in s:
-            if char in dict:
-                stack.append(dict[char])
+
+        for c in s: 
+            if c in hashmap:
+                stack.append(hashmap[c])
+            elif stack and stack[-1] == c:
+                stack.pop()
             else:
-                if stack and stack[-1] == char:
-                    stack.pop()
-                else:
-                    return False
+                return False
         
-        return len(stack) == 0
-
-        # Space Complexity: We create a stack in the worst case will copy all of the string, O(n)
-        # Time Complexity: We need to go through the entire string to check if it is valid, O(n)
-
+        return not stack
         
