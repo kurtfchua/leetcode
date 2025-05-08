4e6c32813519fe67f28a7461cdfd559b66621824
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-08 00:22:51 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (69.94%) - Gitcode

--- 36-valid-sudoku/36. Valid Sudoku.py
+++ 36-valid-sudoku/36. Valid Sudoku.py
@@ -1,35 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # check all rows if unique elements
+        # keep hashmaps for row, cols, and box values
+        # if number is seen before in a row, col, or box hashmap sets we return false
+        # otherwise return true
+        row_map = defaultdict(set)
+        col_map = defaultdict(set)
+        box_map = defaultdict(set)
+
         for i in range(9):
-            row_set = set()
             for j in range(9):
-                if board[i][j] in row_set:
+                if board[i][j] == ".":
+                    continue
+                if (board[i][j] in row_map[i] or
+                    board[i][j] in col_map[j] or
+                    board[i][j] in box_map[(i//3,j//3)]):
                     return False
-                if board[i][j] != ".":
-                    row_set.add(board[i][j])
-        
-        # check all cols if unique elements
-        for j in range(9):
-            col_set = set()
-            for i in range(9):
-                if board[i][j] in col_set:
-                    return False
-                if board[i][j] != ".":
-                    col_set.add(board[i][j])
-        
-        # check each 3x3 box, iterate 3x3 box and see if all unique
-        for i in range(0,9,3):
-            for j in range(0,9,3):
-                box_set = set()
-                for row in range(3):
-                    for col in range(3):
-                        if board[i+row][j+col] in box_set:
-                            return False
-                        if board[i+row][j+col] != ".":
-                            box_set.add(board[i+row][j+col])
+                row_map[i].add(board[i][j])
+                col_map[j].add(board[i][j])
+                box_map[(i//3,j//3)].add(board[i][j])
         
         return True
-
-        # T O(1) - size of input never changes the algorithm it is always 9x9
-        # S O(1) - the sets we create will always be at most length 9
+        
