aea002fbc0dc41ec4cd33a06a3cac3d87760b058
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-09 17:08:16 -0500

    Runtime: 4 ms (9.21%), Memory: 17.9 MB (32.06%) - Gitcode

--- 20-valid-parentheses/20. Valid Parentheses.py
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -1,18 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        hashmap = {
+        mp = {
             '(':')',
-            '[':']',
-            '{':'}'
+            '{':'}',
+            '[':']'
         }
         stack = []
 
         for c in s: 
-            if c in hashmap:
-                stack.append(hashmap[c])
+            if c in mp:
+                stack.append(mp[c])
             elif stack and stack[-1] == c:
                 stack.pop()
             else:
                 return False
         
         return not stack
+        
