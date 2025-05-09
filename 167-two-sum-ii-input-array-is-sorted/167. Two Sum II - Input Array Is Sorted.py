6b0357891bff3b7e61c6ac1bf9d3b50c9c0567e4
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-09 17:01:03 -0500

    Runtime: 7 ms (25.21%), Memory: 18.6 MB (60.14%) - Gitcode

--- 167-two-sum-ii-input-array-is-sorted/167. Two Sum II - Input Array Is Sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167. Two Sum II - Input Array Is Sorted.py
@@ -1,22 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # 2 pointers i, j starting from 0..end of list
-        # if sum == target return [i,j]
-        # if sum < target, increment i 
-        # if sum > target, decrement j
-        i, j = 0, len(numbers) - 1
+        l, r = 0, len(numbers)-1
 
-        while i < j: 
-            sum = numbers[i] + numbers[j]
-            if sum == target:
-                return [i+1, j+1]
-            elif sum < target: 
-                i += 1
+        while l < r:
+            if numbers[l] + numbers[r] == target:
+                return [l+1,r+1]
+            elif numbers[l] + numbers[r] < target:
+                l+=1
             else:
-                j -= 1
+                r-=1
         
-        # T O(n): we iterate through all elements to find pair whose sum == target
-        # S O(1): we don't use any extra data structures 
-        
-    
         
