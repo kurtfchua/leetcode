576e486b01c73ef0870bc5053586e9909666c76d
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-08 21:20:40 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (50.86%) - Gitcode

--- 20-valid-parentheses/20. Valid Parentheses.py
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -3,20 +3,19 @@ class Solution:
         hashmap = {
             '(':')',
             '{':'}',
-            '[':']'
+            '[':']',
         }
         stack = []
 
-        for c in s: 
+        for c in s:
             if c in hashmap:
                 stack.append(hashmap[c])
             elif stack and stack[-1] == c:
-                stack.pop()
+                    stack.pop()
             else:
                 return False
+         
+
         
         return not stack
-
-        # T O(n)
-        # S O(n)
-        
+            
