f5aeaecb7421833ffcc030f52debaed317cb96e6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-09 17:38:31 -0500

    Runtime: 7 ms (89.25%), Memory: 19.3 MB (40.14%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -3,19 +3,14 @@ class Solution:
         l, r = 0, len(height)-1
         max_left, max_right = height[l], height[r]
         count = 0
-        while l < r:
+        while l < r: 
             if max_left <= max_right:
-                l += 1
+                l+=1
                 max_left = max(max_left,height[l])
-                count += max_left - height[l]
+                count += max_left-height[l]
             else:
                 r-=1
                 max_right = max(max_right,height[r])
                 count += max_right-height[r]
-        return count
-
-        # T O(n): we use 2 pointers to reach each element in height at most once
-        # S O(1): we iterate in place using 2 pointers, no new data structures were used
-
-
         
+        return count
