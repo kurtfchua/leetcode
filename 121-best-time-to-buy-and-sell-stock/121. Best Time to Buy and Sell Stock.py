c4cdcf26b82e33f89a0d5dde1a0cbf37264a9ca5
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-09 05:16:37 -0500

    Runtime: 101 ms (46.21%), Memory: 26.9 MB (78.08%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
@@ -1,21 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # iterate through prices list with left and right pointer 0 and 1 respectively
-        # have a maxprofit defaulted to 0 before iteration
-        # while r < len(prices) check if left is smaller than right
-        # if l < r get max between current profit and max profit
-        # else update l to be r and then increment r
-        l, r = 0, 1
-        maxprofit = 0
+        l,r = 0,1
+        max_profit = 0
 
         while r < len(prices):
             if prices[l] < prices[r]:
-                maxprofit = max(maxprofit, prices[r] - prices[l])
+                max_profit = max(max_profit,prices[r]-prices[l])
             else:
                 l = r
             r+=1
-        
-        return maxprofit
+        return max_profit
 
-        # Time Complexity: O(n)
-        # Space Complexity: O(1)
+        
