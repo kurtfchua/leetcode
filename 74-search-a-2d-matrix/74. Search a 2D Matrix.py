5ed6de1af035931882c47b92077e10cac478b1a6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-12 02:49:39 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (99.16%) - Gitcode

--- 74-search-a-2d-matrix/74. Search a 2D Matrix.py
+++ 74-search-a-2d-matrix/74. Search a 2D Matrix.py
@@ -1,21 +1,23 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for sublist in matrix:
-            if self.binary_search(sublist, target): return True
-        
-        return False
-    
-    def binary_search(self, arr, target):
-        left, right = 0, len(arr) - 1
-        
-        while left <= right: 
-            middle = (left + right) // 2
-            if arr[middle] == target:
+        def binary_search(row, target):
+            left, right = 0, len(row)-1 
+            while left <= right:
+                middle = int((left+right)//2)
+                if row[middle] == target:
+                    return True
+                elif row[middle] < target:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+            return False
+
+        for row in matrix:
+            if binary_search(row,target):
                 return True
-            elif arr[middle] < target:
-                left = middle + 1
-            else:
-                right = middle - 1
-        
         return False
-        
+
+                
+
+
+            
