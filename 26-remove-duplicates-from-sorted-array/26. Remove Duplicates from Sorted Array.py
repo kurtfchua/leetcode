4c85c6bdb259fc15b5ce4eec7c78d2cd2a60dfd8
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-12 15:58:12 -0500

    Runtime: 5 ms (18.79%), Memory: 19 MB (48.64%) - Gitcode

--- 26-remove-duplicates-from-sorted-array/26. Remove Duplicates from Sorted Array.py
+++ 26-remove-duplicates-from-sorted-array/26. Remove Duplicates from Sorted Array.py
@@ -1,12 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # pointers i, j will point to next to be replaced and next unqiue element
         i = j = 1
 
         while j < len(nums):
             if nums[j] != nums[j-1]:
-                nums[i] = nums[j]
-                i +=1
+                nums[i]=nums[j]
+                i+=1
             j+=1
         
         return i
+        
         
