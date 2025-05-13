1747d0395915ea4208a4562be2fcd8e45e1111ef
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-12 22:10:51 -0500

    Runtime: 0 ms (100.00%), Memory: 17.6 MB (88.78%) - Gitcode

--- 21-merge-two-sorted-lists/21. Merge Two Sorted Lists.py
+++ 21-merge-two-sorted-lists/21. Merge Two Sorted Lists.py
@@ -5,18 +5,11 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        """
-        Make an initial null dummy node that we will attach to once we start iterating through l1 and l2.
-        We then iterate through both l1 and l2, comparing values. Current.next will be assigned to whichever value in both lists is smaller. We then assign the next value to be compared from whicever we choose.
-        In the case that either list is exhausted first, we attach the rest of the other list to the new list
-        The head of this new list will be refernced by dummy.next
-        Return dummy.next
-        """
-        dummy = ListNode()
+        dummy = ListNode(None)
         current = dummy
 
         while list1 and list2:
-            if list1.val < list2.val:
+            if list1.val <= list2.val:
                 current.next = list1
                 list1 = list1.next
             else:
@@ -25,8 +18,5 @@ class Solution:
             current = current.next
         
         current.next = list1 if list1 else list2
-        
-        return dummy.next
 
-        # S O(1) we create a new linked list from just reassigning pointers of exiting nodes
-        # T O(m+n) we have to go through both lists to create the new list
+        return dummy.next
