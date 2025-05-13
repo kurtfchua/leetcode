026ff8c3c6fcceb2c2e3b10ac3abc6f9991a7ae6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-13 00:19:03 -0500

    Runtime: 43 ms (86.10%), Memory: 20.1 MB (16.48%) - Gitcode

--- 141-linked-list-cycle/141. Linked List Cycle.py
+++ 141-linked-list-cycle/141. Linked List Cycle.py
@@ -6,13 +6,14 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
+        seen = set()
+        current = head
+
+        while current:
+            if current in seen:
                 return True
+            seen.add(current)
+            current = current.next
         
         return False
         
