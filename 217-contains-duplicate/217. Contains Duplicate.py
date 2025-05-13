eb2d04383d637ab67f962bdf5de7a3d7a9e7c5b7
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-13 00:29:34 -0500

    Runtime: 11 ms (63.26%), Memory: 31.6 MB (29.72%) - Gitcode

--- 217-contains-duplicate/217. Contains Duplicate.py
+++ 217-contains-duplicate/217. Contains Duplicate.py
@@ -1,14 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        unique = set()
+        seen = set()
 
         for num in nums:
-            if num in unique:
+            if num in seen:
                 return True
-            unique.add(num)
-
+            seen.add(num)
+        
         return False
-
-        # S O(n) - we pass through nums once and add to the set
-        # T O(n) - we create a set that grows linearly with nums
         
