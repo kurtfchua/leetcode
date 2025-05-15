cc9f4747a638b4b5913f7ab9bb38f43637e331dd
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-15 03:23:10 -0500

    Runtime: 112 ms (81.88%), Memory: 18.1 MB (49.29%) - Gitcode

--- 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.py
+++ 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.py
@@ -7,7 +7,7 @@ class Solution:
         for r in range(len(s)):
             counts[s[r]] = counts.get(s[r],0) + 1
             max_freq = max(max_freq, counts[s[r]])
-            while (r-l+1) - max_freq > k:
+            if (r-l+1) - max_freq > k:
                 counts[s[l]] -= 1
                 l +=1
             res = max(res, r-l+1)
