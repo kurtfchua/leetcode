e7dbef1baf3eb0144f6c0053288475cdbdf08721
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-17 03:21:12 -0500

    Runtime: 2 ms (36.68%), Memory: 19.1 MB (48.06%) - Gitcode

--- 104-maximum-depth-of-binary-tree/104. Maximum Depth of Binary Tree.py
+++ 104-maximum-depth-of-binary-tree/104. Maximum Depth of Binary Tree.py
@@ -7,21 +7,6 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-            return 0
-
-        queue = deque()
-        queue.append(root)
-        levels = 0
-        while queue:
-            for i in range(len(queue)):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            levels += 1
-        
-        return levels
-                
-
+            return 0 
+        return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
         
