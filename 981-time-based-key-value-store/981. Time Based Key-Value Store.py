7b534a149250ad8576c13ecd05c40ad8b82fa95e
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-17 02:55:23 -0500

    Runtime: 101 ms (77.83%), Memory: 74.7 MB (72.30%) - Gitcode

--- 981-time-based-key-value-store/981. Time Based Key-Value Store.py
+++ 981-time-based-key-value-store/981. Time Based Key-Value Store.py
@@ -1,24 +1,26 @@
 class TimeMap:
 
     def __init__(self):
-        self.time_map = defaultdict(list)
+        self.store = defaultdict(list)
         
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.time_map[key].append((value, timestamp))
+        self.store[key].append((value, timestamp))
 
     def get(self, key: str, timestamp: int) -> str:
-        l, r = 0, len(self.time_map[key]) - 1
         res = ""
-
-        while l <= r: 
-            mid = (l+r) // 2
-            if self.time_map[key][mid][1] <= timestamp:
-                res = self.time_map[key][mid][0]
+        ts = self.store[key]
+        l, r = 0, len(ts)-1
+        
+        while l <= r:
+            mid = (l+r)//2
+            if ts[mid][1] <= timestamp:
+                res = ts[mid][0]
                 l = mid + 1
             else:
                 r = mid - 1
-
         return res
+
+        
 
 
 # Your TimeMap object will be instantiated and called as such:
