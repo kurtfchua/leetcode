6ddfdddaf5f6e443099a498c6581b8e2fed496a6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-20 00:07:21 -0500

    Runtime: 0 ms (100.00%), Memory: 19.9 MB (8.19%) - Gitcode

--- 98-validate-binary-search-tree/98. Validate Binary Search Tree.py
+++ 98-validate-binary-search-tree/98. Validate Binary Search Tree.py
@@ -14,4 +14,22 @@ class Solution:
             
             return dfs(root.left, min_level, root.val) and dfs(root.right, root.val, max_level)
         
-        return dfs(root, float('-inf'), float('inf'))
+        def bfs(root):
+            queue = deque()
+            min_val, max_val = float('-inf'), float('inf')
+            queue.append((root, min_val, max_val))
+
+            while queue: 
+                for i in range(len(queue)):
+                    node, min_val, max_val = queue.popleft()
+                    if node:
+                        if not (min_val < node.val < max_val):
+                            return False
+                        
+                        queue.append((node.left, min_val, node.val))
+                        queue.append((node.right, node.val, max_val))
+            
+            return True
+
+        #return dfs(root, float('-inf'), float('inf'))
+        return bfs(root)
