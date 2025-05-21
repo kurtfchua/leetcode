f3922f192ae9192568bc7eb59704ca2093eb3992
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-20 21:33:30 -0500

    Runtime: 0 ms (100.00%), Memory: 19.4 MB (67.19%) - Gitcode

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105. Construct Binary Tree from Preorder and Inorder Traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -6,11 +6,30 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder: return None
+        def bf(preorder, inorder):
+            if not preorder or not inorder: return None
 
-        root = TreeNode(preorder[0])
-        root_idx = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:root_idx+1], inorder[:root_idx])
-        root.right = self.buildTree(preorder[root_idx+1:], inorder[root_idx+1:])
+            root = TreeNode(preorder[0])
+            root_idx = inorder.index(preorder[0])
+            root.left = self.buildTree(preorder[1:root_idx+1], inorder[:root_idx])
+            root.right = self.buildTree(preorder[root_idx+1:], inorder[root_idx+1:])
+            
 
-        return root        
+            return root        
+        
+        hashmap = {val: i for i, val in enumerate(inorder)}
+        self.preorder_idx = 0 
+        def dfs(l, r):
+            if l > r:
+                return None
+
+            root_val = preorder[self.preorder_idx]
+            self.preorder_idx += 1
+            root = TreeNode(root_val)
+            inorder_idx = hashmap[root_val]
+            root.left = dfs(l, inorder_idx-1)
+            root.right = dfs(inorder_idx+1, r)
+
+            return root
+        
+        return dfs(0, len(inorder)-1)
