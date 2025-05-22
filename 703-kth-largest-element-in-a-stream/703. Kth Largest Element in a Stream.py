35bf74b18b67036d52caceb3f40b9dd3570ef048
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-21 22:13:42 -0500

    Runtime: 21 ms (36.87%), Memory: 23.7 MB (62.89%) - Gitcode

--- 703-kth-largest-element-in-a-stream/703. Kth Largest Element in a Stream.py
+++ 703-kth-largest-element-in-a-stream/703. Kth Largest Element in a Stream.py
@@ -2,51 +2,18 @@ class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.heap = []
-
-        for num in nums:
-            self.push(num)
+        self.heap = nums
+        heapq.heapify(self.heap)
 
         while len(self.heap) > self.k:
-            self.pop()
-
-    def push(self, val):
-        self.heap.append(val)
-        i = len(self.heap) - 1
-
-        while i > 0 and self.heap[(i-1)//2] > self.heap[i]:
-            self.heap[(i-1)//2], self.heap[i] = self.heap[i], self.heap[(i-1)//2]
-            i = (i-1) // 2
-    
-    def pop(self):
-        if len(self.heap) == 0: return self.heap
-        if len(self.heap) == 1: return self.heap.pop()
-
-        res = self.heap[0]
-        self.heap[0] = self.heap.pop()
-
-        i = 0
-
-        while 2*i + 1 < len(self.heap):
-            if (2*i + 2 < len(self.heap) and
-                self.heap[2*i+2] < self.heap[2*i+1] and
-                self.heap[2*i+2] < self.heap[i]):
-                self.heap[2*i+2], self.heap[i] = self.heap[i], self.heap[2*i+2]
-                i = 2*i+2
-            elif self.heap[2*i+1] < self.heap[i]:
-                self.heap[2*i+1], self.heap[i] = self.heap[i], self.heap[2*i+1]
-                i = 2*i+1
-            else:
-                break
-        
-        return res
-
+            heapq.heappop(self.heap)
 
     def add(self, val: int) -> int:
-        self.push(val)
+        heapq.heappush(self.heap, val)
+
         while len(self.heap) > self.k:
-            self.pop()
-        
+            heapq.heappop(self.heap)      
+
         return self.heap[0]
 
 # Your KthLargest object will be instantiated and called as such:
