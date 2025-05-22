83194f8f3865343b1ee42d1fd64484db9a0b4272
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-21 23:03:59 -0500

    Runtime: 249 ms (56.11%), Memory: 20.2 MB (58.29%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,23 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        count = 0
-        def bfs(r,c):
-            queue = deque()
-            queue.append((r,c))
-            grid[r][c] = "0"
-            while queue:
-                for _ in range(len(queue)):
+
+        def bfs(r, c):
+            queue = deque([(r,c)])
+            grid[r][c] == "0"
+
+            while queue: 
+                for i in range(len(queue)):
                     r, c = queue.popleft()
-                    directions = [(0,1),(1,0),(-1,0),(0,-1)]
-                    for dr, dc in directions:
+                    directions = [(0,1),(1,0),(0,-1),(-1,0)]
+                    for dr, dc in directions: 
                         if 0 <= r+dr < rows and 0 <= c+dc < cols and grid[r+dr][c+dc] == "1":
-                            queue.append((r+dr,c+dc))
-                            grid[r+dr][c+dc] = "0"
+                            queue.append((r+dr, c+dc))
+                            grid[r+dr][c+dc] = "0" 
+       
+        num_islands = 0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
                     bfs(r,c)
-                    count += 1
-        return count
-        
+                    num_islands += 1
+
+        return num_islands       
