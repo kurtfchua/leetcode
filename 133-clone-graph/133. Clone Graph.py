d46d56b358d383b09eb55abc9145f8d7910b06a4
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-22 00:08:51 -0500

    Runtime: 42 ms (66.44%), Memory: 18.3 MB (39.32%) - Gitcode

---
+++ 133-clone-graph/133. Clone Graph.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return None
+        
+        hashmap = {}
+
+        def bfs(node):
+            queue = deque([node])
+            hashmap[node] = Node(node.val)
+
+            while queue: 
+                for _ in range(len(queue)):
+                    node = queue.popleft()
+                    for neighbor in node.neighbors: 
+                        if neighbor not in hashmap:
+                            hashmap[neighbor] = Node(neighbor.val)
+                            queue.append(neighbor)
+                        hashmap[node].neighbors.append(hashmap[neighbor])
+
+        bfs(node)
+        return hashmap[node]
