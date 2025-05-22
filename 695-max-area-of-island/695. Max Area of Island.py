e78a3756d7257d0681f38f42b9402e812415d98c
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-21 23:21:34 -0500

    Runtime: 23 ms (58.12%), Memory: 17.9 MB (93.47%) - Gitcode

--- 695-max-area-of-island/695. Max Area of Island.py
+++ 695-max-area-of-island/695. Max Area of Island.py
@@ -1,29 +1,29 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        max_area = 0
-        
+
         def bfs(r, c):
-            queue = deque()
-            queue.append((r,c))
+            queue = deque([(r,c)])
             grid[r][c] = 0
             area = 1
 
-            while queue:
-                for _ in range(len(queue)):
+            while queue: 
+                for i in range(len(queue)):
                     r, c = queue.popleft()
                     directions = [(0,1),(1,0),(0,-1),(-1,0)]
-                    for dr, dc in directions:
+                    for dr, dc in directions: 
                         if 0 <= r+dr < rows and 0 <= c+dc < cols and grid[r+dr][c+dc] == 1:
-                            queue.append((r+dr,c+dc))
-                            area += 1
+                            queue.append((r+dr, c+dc))
                             grid[r+dr][c+dc] = 0
+                            area += 1
             return area
-        
+        max_area = 0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    max_area = max(max_area, bfs(r,c))
-
+                    max_area = max(max_area, bfs(r, c))
+        
         return max_area
+        
+                            
         
