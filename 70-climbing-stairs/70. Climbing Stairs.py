189c35f97b314e7cf14c1e1f7238db776be09c2c
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-22 20:57:31 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (48.53%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -1,6 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        memo = {}
+        def dfs(n):
+            if n <= 2: return n 
 
+            if n in memo: return memo[n]
+
+            memo[n] = dfs(n-1) + dfs(n-2)
+
+            return memo[n]
+       
         def dp(n):
             if n <= 2:
                 return n
@@ -11,7 +20,8 @@ class Solution:
                 dp[0], dp[1] = dp[1], dp[1] + dp[0]
                 i += 1
             return dp[1]
-        return dp(n)
+        return dfs(n)
+        #return dp(n)
         
         # T O(n): we memo each result after exploring the longest branch. each subproblem is solved once
         # S O(n): n is the maximum depth of the tree we travel when collapsing the call stack
