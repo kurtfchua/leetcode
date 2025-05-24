743a069e2c3070bcda03651b4d014e3895349ab4
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-24 02:11:15 -0500

    Runtime: 61 ms (82.44%), Memory: 19.3 MB (5.49%) - Gitcode

--- 621-task-scheduler/621. Task Scheduler.py
+++ 621-task-scheduler/621. Task Scheduler.py
@@ -1,24 +1,22 @@
 class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int: 
-        hashmap = Counter(tasks)
-        heap = [-value for value in hashmap.values()]
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_freq = Counter(tasks)
+        heap = [-val for val in task_freq.values()]
         heapq.heapify(heap)
-
 
         queue = deque()
         time = 0
 
         while heap or queue: 
             time += 1
-
             if heap: 
-                count = heapq.heappop(heap) + 1
+                count = 1 + heapq.heappop(heap)
                 if count: 
                     queue.append((count, time + n))
-            else:
+            else: 
                 time = queue[0][1]
-            
+
             if queue and queue[0][1] == time: 
-                heapq.heappush(heap, queue.popleft()[0])
-        
-        return time
+                heapq.heappush(heap, queue.popleft()[0]) 
+
+        return time       
