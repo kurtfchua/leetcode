fece45255054e271e2c55d5a54b94e1c94cf0bd6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-25 03:49:39 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (34.49%) - Gitcode

--- 551-student-attendance-record-i/551. Student Attendance Record I.py
+++ 551-student-attendance-record-i/551. Student Attendance Record I.py
@@ -1,10 +1,8 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        record_counts = Counter(s)
-
-        if record_counts['A'] >= 2: return False
+        if s.count('A') >= 2: return False
         if 'LLL' in s: return False
-        
+
         return True
 
 
