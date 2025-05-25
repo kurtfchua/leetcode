4e54e018563dfb7622bfc752bf650c126e4a30d6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-24 20:44:29 -0500

    Runtime: 3320 ms (78.23%), Memory: 18.1 MB (20.97%) - Gitcode

--- 79-word-search/79. Word Search.py
+++ 79-word-search/79. Word Search.py
@@ -2,30 +2,29 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
 
-        def dfs(r, c, i):
-            if i == len(word): return True
-            if (r < 0 or r >= rows or
-               c < 0 or c >= cols or 
-               board[r][c] != word[i] or
-               board[r][c] == "#"):
-                    return False
-
-            temp = board[r][c]
+        def dfs(r,c,i):
+            if i == len(word):
+                return True
+            if (0 > r or r >= rows or
+                0 > c or c >= cols or
+                board[r][c] == "#" or
+                board[r][c] != word[i]):
+                return False
+            
+            tmp = board[r][c]
             board[r][c] = "#"
-            found_word = (
-                dfs(r+1, c, i+1) or
-                dfs(r, c+1, i+1) or
-                dfs(r-1, c, i+1) or
-                dfs(r, c-1, i+1)
-                )
-            board[r][c] = temp
+            word_exists = (dfs(r+1, c, i+1) or 
+                           dfs(r, c+1, i+1) or 
+                           dfs(r-1, c, i+1) or
+                           dfs(r, c-1, i+1))
+            board[r][c] = tmp
 
-            return found_word
+            return word_exists
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == word[0]: 
-                    if dfs(r,c,0):
+                if board[r][c] == word[0]:
+                    if dfs(r,c,0): 
                         return True
-
-        return False        
+        
+        return False
