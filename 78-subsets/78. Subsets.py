ca3aa5f9cbb8560d41024cf2110a133db649a7d5
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-25 00:09:50 -0500

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (98.91%) - Gitcode

--- 78-subsets/78. Subsets.py
+++ 78-subsets/78. Subsets.py
@@ -6,14 +6,15 @@ class Solution:
         def dfs(i):
             if i == len(nums):
                 res.append(subset.copy())
-                return
+                return 
             
             subset.append(nums[i])
-            dfs(i + 1)
+            dfs(i+1)
 
             subset.pop()
             dfs(i+1)
+        
         dfs(0)
-    
+
         return res
         
