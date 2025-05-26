bc5bd3b4c67d23a9f4ba6b3b4d0dc631f5168fd6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-25 23:35:29 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (57.50%) - Gitcode

--- 46-permutations/46. Permutations.py
+++ 46-permutations/46. Permutations.py
@@ -1,14 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0: return [[]]
+        def recur(nums):
+            if len(nums) == 0: return [[]]
 
-        permutations = self.permute(nums[1:])
-        res = []
+            permutations = self.permute(nums[1:])
+            res = []
 
-        for perm in permutations:
-            for i in range(len(perm)+1):
-                perm_copy = perm.copy()
-                perm_copy.insert(i, nums[0])
-                res.append(perm_copy)
+            for perm in permutations:
+                for i in range(len(perm)+1):
+                    perm_copy = perm.copy()
+                    perm_copy.insert(i, nums[0])
+                    res.append(perm_copy)
 
-        return res        
+            return res 
+
+        return recur(nums)       
