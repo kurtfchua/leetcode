23f1664f4848ab3223311211dfb14b005da7749c
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-25 22:18:37 -0500

    Runtime: 3399 ms (76.26%), Memory: 18 MB (20.92%) - Gitcode

--- 79-word-search/79. Word Search.py
+++ 79-word-search/79. Word Search.py
@@ -2,29 +2,32 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
 
-        def dfs(r,c,i):
+        def dfs(r, c, i):
             if i == len(word):
                 return True
-            if (0 > r or r >= rows or
-                0 > c or c >= cols or
+            
+            if (r < 0 or r >= rows or 
+                c < 0 or c >= cols or 
                 board[r][c] == "#" or
                 board[r][c] != word[i]):
                 return False
-            
+
             tmp = board[r][c]
             board[r][c] = "#"
-            word_exists = (dfs(r+1, c, i+1) or 
-                           dfs(r, c+1, i+1) or 
-                           dfs(r-1, c, i+1) or
-                           dfs(r, c-1, i+1))
-            board[r][c] = tmp
 
-            return word_exists
+            up = dfs(r-1, c, i+1)
+            down = dfs(r+1, c, i+1)
+            left = dfs(r, c-1, i+1)
+            right = dfs(r, c+1, i+1)
+
+            board[r][c] = tmp
+            
+            return up or down or left or right
         
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == word[0]:
-                    if dfs(r,c,0): 
+                    if dfs(r, c, 0):
                         return True
         
         return False
