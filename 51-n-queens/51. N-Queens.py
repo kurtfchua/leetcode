b8922b80e83f21634d3ae6a4a1c06f5318c65925
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-27 01:51:42 -0500

    Runtime: 7 ms (97.40%), Memory: 18.5 MB (15.02%) - Gitcode

---
+++ 51-n-queens/51. N-Queens.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col_set = set()
+        pos_diag = set()
+        neg_diag = set()
+
+        board = [["."] * n for _ in range(n)]
+        res = []
+
+        def backtrack(r):
+            if r == n: 
+                copy = ["".join(word) for word in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col_set or (r+c) in pos_diag or (r-c) in neg_diag: 
+                    continue 
+
+                col_set.add(c)
+                pos_diag.add(r+c)
+                neg_diag.add(r-c)
+                board[r][c] = "Q"
+                
+                backtrack(r+1) 
+
+                col_set.remove(c)
+                pos_diag.remove(r+c)
+                neg_diag.remove(r-c)
+                board[r][c] = "."
+       
+        backtrack(0)
+
+        return res
