56a9137baa3b2359e7287424fa2b0b49f43a3084
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-27 02:15:41 -0500

    Runtime: 10 ms (79.44%), Memory: 18.2 MB (82.58%) - Gitcode

--- 51-n-queens/51. N-Queens.py
+++ 51-n-queens/51. N-Queens.py
@@ -4,31 +4,33 @@ class Solution:
         pos_diag = set()
         neg_diag = set()
 
-        board = [["."] * n for _ in range(n)]
+        board = [["."]*n for _ in range(n)]
         res = []
 
         def backtrack(r):
             if r == n: 
                 copy = ["".join(word) for word in board]
                 res.append(copy)
-                return
-
+                return 
+            
             for c in range(n):
                 if c in col_set or (r+c) in pos_diag or (r-c) in neg_diag: 
-                    continue 
+                    continue
 
                 col_set.add(c)
                 pos_diag.add(r+c)
                 neg_diag.add(r-c)
                 board[r][c] = "Q"
-                
-                backtrack(r+1) 
 
+                backtrack(r+1)
+                
                 col_set.remove(c)
                 pos_diag.remove(r+c)
                 neg_diag.remove(r-c)
                 board[r][c] = "."
-       
+        
         backtrack(0)
 
         return res
+
+
