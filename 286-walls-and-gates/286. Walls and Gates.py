7d8a473d126042701261cd2f7ad506b86d3dc286
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-27 19:46:42 -0500

    Runtime: 59 ms (38.28%), Memory: 24.9 MB (25.17%) - Gitcode

--- 286-walls-and-gates/286. Walls and Gates.py
+++ 286-walls-and-gates/286. Walls and Gates.py
@@ -9,19 +9,22 @@ class Solution:
 
         for r in range(rows):
             for c in range(cols):
-                if rooms[r][c] == 0: 
+                if rooms[r][c] == 0:
                     queue.append((r,c))
                     visited.add((r,c))
-        level = 1
+            
+        distance = 0
         while queue: 
             for i in range(len(queue)):
                 r, c = queue.popleft()
-              
-                directions = [(0,1), (1,0), (0,-1), (-1,0)]
+                rooms[r][c] = distance
+                directions = [(0,1),(1,0),(0,-1),(-1,0)]
                 for dr, dc in directions: 
-                    if (0 <= r+dr < rows and 0 <= c+dc < cols and 
-                    rooms[r+dr][c+dc] != -1 and (r+dr,c+dc) not in visited): 
-                        rooms[r+dr][c+dc] = level
-                        visited.add((r+dr,c+dc))
+                    if (0 <= r+dr < rows and 0 <= c+dc < cols and
+                        rooms[r+dr][c+dc] != -1 and 
+                        (r+dr,c+dc) not in visited):
                         queue.append((r+dr,c+dc))
-            level += 1
+                        visited.add((r+dr,c+dc))
+            distance += 1
+
+
