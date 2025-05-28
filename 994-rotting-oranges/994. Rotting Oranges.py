9047d7c301127232563000bf1e4ebf19044b3c15
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-27 20:03:45 -0500

    Runtime: 3 ms (82.22%), Memory: 18 MB (26.99%) - Gitcode

--- 994-rotting-oranges/994. Rotting Oranges.py
+++ 994-rotting-oranges/994. Rotting Oranges.py
@@ -9,24 +9,24 @@ class Solution:
                 if grid[r][c] == 2:
                     queue.append((r,c))
                     visited.add((r,c))
-        
         minutes = -1
-        while queue:
+        while queue: 
             for i in range(len(queue)):
                 r, c = queue.popleft()
-                directions = [(0,1),(1,0),(-1,0),(0,-1)]
+                directions = [(0,1), (1,0), (0,-1), (-1,0)]
                 for dr, dc in directions: 
-                    if (0<= r+dr < rows and 0 <= c+dc <cols and 
-                        (r+dr, c+dc) not in visited and 
-                        grid[r+dr][c+dc] == 1):
-                        queue.append((r+dr, c+dc))
-                        visited.add((r+dr, c+dc))
-                        grid[r+dr][c+dc] = 2
+                    if 0 <= r+dr < rows and 0 <= c+dc < cols and grid[r+dr][c+dc] == 1: 
+                        grid[r+dr][c+dc] = 2 
+                        queue.append((r+dr,c+dc))
+                        visited.add((r+dr, c+dc)) 
             minutes += 1
         
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
+                if grid[r][c] == 1: 
                     return -1
-        
+
         return minutes if minutes > 0 else 0
+
+        
+        
