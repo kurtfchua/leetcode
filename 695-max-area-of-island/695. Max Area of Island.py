0b94d62f0bc7a0751c06aab633648c506c87b649
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-27 19:18:38 -0500

    Runtime: 23 ms (57.63%), Memory: 18 MB (93.21%) - Gitcode

--- 695-max-area-of-island/695. Max Area of Island.py
+++ 695-max-area-of-island/695. Max Area of Island.py
@@ -10,20 +10,20 @@ class Solution:
             while queue: 
                 for i in range(len(queue)):
                     r, c = queue.popleft()
-                    directions = [(0,1),(1,0),(0,-1),(-1,0)]
+                    directions = [(0,1), (1,0), (-1,0), (0,-1)]
                     for dr, dc in directions: 
-                        if 0 <= r+dr < rows and 0 <= c+dc < cols and grid[r+dr][c+dc] == 1:
-                            queue.append((r+dr, c+dc))
+                        if 0 <= r +dr < rows and 0 <= c + dc < cols and grid[r+dr][c+dc] == 1:
+                            queue.append((r+dr,c+dc))
                             grid[r+dr][c+dc] = 0
                             area += 1
+            
             return area
+        
         max_area = 0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    max_area = max(max_area, bfs(r, c))
+                    max_area = max(max_area, bfs(r,c))
         
         return max_area
-        
-                            
         
