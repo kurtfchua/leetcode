1a5e9c26ba34d9c0f250b10299b4521e070d45c6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-30 01:01:07 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (7.94%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -1,27 +1,24 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-        def dfs(n):
-            if n <= 2: return n 
 
-            if n in memo: return memo[n]
-
-            memo[n] = dfs(n-1) + dfs(n-2)
-
-            return memo[n]
-       
-        def dp(n):
-            if n <= 2:
+        def basic(n):
+            if n <= 2: 
                 return n
-            dp = [1,2]
-            i = 3
-
-            while i <= n:
-                dp[0], dp[1] = dp[1], dp[1] + dp[0]
-                i += 1
-            return dp[1]
-        return dfs(n)
-        #return dp(n)
+            
+            return basic(n-1) + basic(n-2)
         
-        # T O(n): we memo each result after exploring the longest branch. each subproblem is solved once
-        # S O(n): n is the maximum depth of the tree we travel when collapsing the call stack
+        mem = {}
+        def top_down(n):
+            if n <= 2: 
+                return n 
+            
+            if n in mem: 
+                return mem[n]
+                
+            mem[n] = top_down(n-1) + top_down(n-2)
+
+            return mem[n]
+
+        return top_down(n)
+        #return basic(n)
+        
