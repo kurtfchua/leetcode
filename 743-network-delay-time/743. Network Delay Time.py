33a298a1ecf63b60acc92c142e8a0d9a433e8819
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-30 01:11:57 -0500

    Runtime: 388 ms (45.02%), Memory: 19.8 MB (35.34%) - Gitcode

--- 743-network-delay-time/743. Network Delay Time.py
+++ 743-network-delay-time/743. Network Delay Time.py
@@ -2,20 +2,23 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adj_list = defaultdict(list)
 
-        for src, dst, time in times: 
-            adj_list[src].append((dst, time))
-        
-        min_heap = [(0, k)]
+        for u, v, w in times: 
+            adj_list[u].append((v,w))
+
+        heap = [(0, k)]
         shortest_paths = {}
 
-        while min_heap: 
-            time1, node = heapq.heappop(min_heap) 
-            if node in shortest_paths: 
+        while heap: 
+            w1, u1 = heapq.heappop(heap)
+            if u1 in shortest_paths: 
                 continue
-            shortest_paths[node] = time1
+            
+            shortest_paths[u1] = w1
 
-            for dst, time2 in adj_list[node]:
-                if dst not in shortest_paths: 
-                    heapq.heappush(min_heap, (time2 + time1, dst))
-
-        return max(shortest_paths.values()) if len(shortest_paths) == n else -1       
+            for u2, w2 in adj_list[u1]:
+                if u2 not in shortest_paths:
+                    heapq.heappush(heap, (w2+w1, u2))
+     
+        
+        return max(shortest_paths.values()) if len(shortest_paths) == n else -1
+                     
