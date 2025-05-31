c10cbc633a77444b50d7df02fbed2154da38912f
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-30 22:45:15 -0500

    Runtime: 0 ms (100.00%), Memory: 17.7 MB (48.49%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -17,7 +17,20 @@ class Solution:
             mem[n] = top_bottom(n-1) + top_bottom(n-2)
 
             return mem[n]
+        
+        def bottom_up(n):
+            if n <= 2:
+                return n 
+            
+            a, b = 1, 2
+            i = 3
+            while i <= n:
+                a, b = b, a+b
+                i += 1
+
+            return b 
 
         #return basic(n)
-        return top_bottom(n)
+        #return top_bottom(n)
+        return bottom_up(n)
         
