0f637d94afa3dc8be29d99caf98f3942dfdbd49a
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-30 22:27:16 -0500

    Runtime: 338 ms (25.53%), Memory: 17.8 MB (23.81%) - Gitcode

--- 509-fibonacci-number/509. Fibonacci Number.py
+++ 509-fibonacci-number/509. Fibonacci Number.py
@@ -1,11 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # base case
-        if n <=1: return n
 
-        # call fib on both n-1 and n-2 and get sum
-        return self.fib(n-1) + self.fib(n-2)
+        def basic(n):
+            if n <= 1: 
+                return n 
 
-        # T O(2^n): we add a new level of 2 new nodes to the recursion tree with each call
-        # S O(n): n is the maximum depth of the tree we travel when collapsing the call stack
+            return basic(n-2) + basic(n-1)
+        
+        return basic(n)
         
