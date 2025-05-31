a59d2725380ec1028b75107512cfa56b366e00c2
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-05-30 22:31:00 -0500

    Runtime: 29 ms (95.67%), Memory: 17.8 MB (41.29%) - Gitcode

--- 509-fibonacci-number/509. Fibonacci Number.py
+++ 509-fibonacci-number/509. Fibonacci Number.py
@@ -18,9 +18,23 @@ class Solution:
 
             return mem[n]
 
+        def bottom_up(n):
+            if n <= 1: 
+                return n 
+            
+            a, b = 0, 1
+            i = 2
+
+            while i <= n: 
+                a, b = b, a+b
+                i += 1
+            
+            return b
+
 
         #return basic(n)
-        return top_down(n)
+        #return top_down(n)
+        return bottom_up(n)
 
 
         
