a6e17cafbd7889d7b10807bf3038605c932a018c
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-01 01:29:21 -0500

    Runtime: 88 ms (34.68%), Memory: 32.8 MB (22.73%) - Gitcode

--- 53-maximum-subarray/53. Maximum Subarray.py
+++ 53-maximum-subarray/53. Maximum Subarray.py
@@ -1,14 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        cur_sum = 0
         max_sum = nums[0]
-        curr_sum = 0
 
         for num in nums: 
-            curr_sum = num + max(curr_sum, 0)
-            max_sum = max(max_sum, curr_sum)
+            cur_sum  = num + max(cur_sum, 0)
+            max_sum = max(max_sum, cur_sum)
         
         return max_sum
-
-
-        # T O(n) S O(1)
         
