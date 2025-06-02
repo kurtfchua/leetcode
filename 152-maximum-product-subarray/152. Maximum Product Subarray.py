26a2c3de7dc24b7f1e571bb13f6a1721d63a398b
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-02 09:15:08 -0500

    Runtime: 11 ms (51.98%), Memory: 18.4 MB (52.74%) - Gitcode

--- 152-maximum-product-subarray/152. Maximum Product Subarray.py
+++ 152-maximum-product-subarray/152. Maximum Product Subarray.py
@@ -1,12 +1,26 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        prefix = suffix = 0
-        n, res = len(nums), nums[0]
+        def find_pref_suff(nums):
+            prefix = suffix = 0
+            n, res = len(nums), nums[0]
 
-        for i in range(n):
-            prefix = nums[i] * (prefix or 1)
-            suffix = nums[n-1-i] * (suffix or 1)
-            res = max(res, max(prefix, suffix))
+            for i in range(n):
+                prefix = nums[i] * (prefix or 1)
+                suffix = nums[n-1-i] * (suffix or 1)
+                res = max(res, max(prefix, suffix))
 
-        return res
+            return res
         
+        def dp(nums):
+            curr_max, curr_min = 1, 1
+            max_prod = nums[0]
+
+            for num in nums:
+                tmp = curr_max * num
+                curr_max = max(num*curr_max, num*curr_min, num)
+                curr_min = min(tmp, num*curr_min, num) 
+                max_prod = max(max_prod, curr_max)
+            
+            return max_prod
+            
+        return dp(nums)
