df980604d2643d1569e5a9ca366c6b7fdbcf0557
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-02 18:42:55 -0500

    Runtime: 4 ms (41.47%), Memory: 18.8 MB (51.01%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        comps = {}
+        hashmap = {}
 
         for i in range(len(nums)):
-            if nums[i] in comps:
-                return [i, comps[nums[i]]]
-            else:
-                comps[target-nums[i]] = i
+            if target-nums[i] in hashmap:
+                return [hashmap[target-nums[i]], i]
+            else: 
+                hashmap[nums[i]]= i
         
-        # T O(n) - we must iterate through nums to find the 2 pairs and build comps hashmap
-        # S O(n) - the hashmap grows with our nums list
+
+        
