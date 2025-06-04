08e134ce5dff2c25e762a398010b4982cc711614
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-03 20:49:06 -0500

    Runtime: 0 ms (100.00%), Memory: 19 MB (33.82%) - Gitcode

--- 1-two-sum/1. Two Sum.py
+++ 1-two-sum/1. Two Sum.py
@@ -5,8 +5,6 @@ class Solution:
         for i in range(len(nums)):
             if target-nums[i] in hashmap:
                 return [hashmap[target-nums[i]], i]
-            else: 
-                hashmap[nums[i]]= i
+            hashmap[nums[i]] = i
         
-
         
