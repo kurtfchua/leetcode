fc277dd55b083149985588c2a94f8f9abc552ae7
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-03 22:55:01 -0500

    Runtime: 612 ms (38.60%), Memory: 20.7 MB (69.07%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -15,6 +15,9 @@ class Solution:
                     l += 1
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
+                    r -= 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
                 elif total < 0: 
                     l += 1
                 else:
