4d616a0b63f41071ae43f34699bbb59dac211a67
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-03 23:37:50 -0500

    Runtime: 79 ms (26.11%), Memory: 18.3 MB (39.93%) - Gitcode

--- 271-encode-and-decode-strings/271. Encode and Decode Strings.py
+++ 271-encode-and-decode-strings/271. Encode and Decode Strings.py
@@ -2,29 +2,30 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        encoded = []
-        for word in strs:
-            encoded.append(str(len(word))+"#"+word)
-        return ''.join(encoded)
+        # for every word append #len(word)word to result string
+        res = ""
+        for word in strs: 
+            res += str(len(word)) +"#" +  word
+
+        return res
     
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        res = []    
+        # iterate through word, find index of # get length the next index, slice string from number to len(word) update i
+        res = []
         i = j = 0
-
         while i < len(s):
             while s[j] != "#":
-                j+=1
-            length = int(s[i:j])
-            decoded_word = s[j+1:j+length+1]
-            res.append(decoded_word)
-            i=j+length+1
-            j=i
-        
-        return res
+                j += 1
 
-    # 
+            length = int(s[i:j])
+            word = s[j+1:j+length+1] 
+            res.append(word)
+            i = j+length+1
+            j = i 
+
+        return res       
 
 
 # Your Codec object will be instantiated and called as such:
