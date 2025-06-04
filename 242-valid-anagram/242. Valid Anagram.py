577ec9a7abc82154781345922461fd89c1becc92
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-03 22:20:21 -0500

    Runtime: 18 ms (19.19%), Memory: 18.5 MB (19.56%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -1,14 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-
-        s_chars = [0]*26
-        t_chars = [0]*26
-
-        for c in s: 
-            s_chars[ord(c)-ord('a')] += 1
+        return sorted(s) == sorted(t)
         
-        for c in t: 
-            t_chars[ord(c)-ord('a')] += 1
-
-        return s_chars == t_chars
