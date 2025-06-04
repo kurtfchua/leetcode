855ffe7560667df96bc6757b06af2038df40e874
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-04 00:22:49 -0500

    Runtime: 2 ms (85.86%), Memory: 18 MB (24.46%) - Gitcode

--- 36-valid-sudoku/36. Valid Sudoku.py
+++ 36-valid-sudoku/36. Valid Sudoku.py
@@ -1,21 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows_set = defaultdict(set)
-        cols_set = defaultdict(set)
-        boxes_set = defaultdict(set)
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".": 
+                if board[r][c] == ".":
                     continue
-
-                if (board[r][c] in rows_set[r] or 
-                    board[r][c] in cols_set[c] or 
-                    board[r][c] in boxes_set[(r//3, c//3)]):
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[(r//3,c//3)]:
                     return False
-
-                rows_set[r].add(board[r][c])
-                cols_set[c].add(board[r][c])
-                boxes_set[(r//3,c//3)].add(board[r][c])
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                boxes[(r//3,c//3)].add(board[r][c])
         
         return True
