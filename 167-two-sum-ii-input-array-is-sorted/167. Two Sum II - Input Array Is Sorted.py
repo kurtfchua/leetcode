714e2bbaa621b5c69135b3c55243d3a41c185cee
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-04 23:04:42 -0500

    Runtime: 4 ms (48.36%), Memory: 18.7 MB (9.58%) - Gitcode

--- 167-two-sum-ii-input-array-is-sorted/167. Two Sum II - Input Array Is Sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167. Two Sum II - Input Array Is Sorted.py
@@ -3,9 +3,10 @@ class Solution:
         l, r = 0, len(numbers)-1
 
         while l < r: 
-            if numbers[l] + numbers[r] == target:
+            if numbers[l] + numbers[r] == target: 
                 return [l+1, r+1]
-            elif numbers[l] + numbers[r] < target: 
+            elif numbers[l] + numbers[r] < target:
                 l += 1
-            else:
+            else: 
                 r -= 1
+        
