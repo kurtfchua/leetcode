f11e2c255aa61ee30b69475d9cee863fba6ff04e
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-05 02:02:04 -0500

    Runtime: 3 ms (82.20%), Memory: 18 MB (24.58%) - Gitcode

--- 36-valid-sudoku/36. Valid Sudoku.py
+++ 36-valid-sudoku/36. Valid Sudoku.py
@@ -6,12 +6,13 @@ class Solution:
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+                if board[r][c] == ".": 
                     continue
-                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[(r//3,c//3)]:
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[(r//3, c//3)]:
                     return False
+                
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
-                boxes[(r//3,c//3)].add(board[r][c])
+                boxes[(r//3, c//3)].add(board[r][c])
         
         return True
