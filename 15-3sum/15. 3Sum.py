1b874f1c8a32802182f4f9b061aaee06fae0177b
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-04 23:15:30 -0500

    Runtime: 841 ms (23.68%), Memory: 20.7 MB (69.00%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -3,14 +3,12 @@ class Solution:
         nums.sort()
         res = []
 
-        for i in range(len(nums)): 
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
-            l, r = i+1, len(nums)-1
+            l, r = i + 1, len(nums)-1
             while l < r: 
-                total = nums[i] + nums[l] + nums[r]
-                if total == 0: 
+                if nums[i] + nums[l] + nums[r] == 0:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
                     while l < r and nums[l] == nums[l-1]:
@@ -18,9 +16,11 @@ class Solution:
                     r -= 1
                     while l < r and nums[r] == nums[r+1]:
                         r -= 1
-                elif total < 0: 
+                elif nums[i] + nums[l] + nums[r] < 0:
                     l += 1
                 else:
-                    r-= 1
+                    r -= 1
         
         return res
+
+        
