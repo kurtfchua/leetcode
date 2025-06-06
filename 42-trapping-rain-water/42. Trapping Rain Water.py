122e7fed64d0f772541995ae301248bf355b0eef
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-06 02:57:04 -0500

    Runtime: 8 ms (80.56%), Memory: 19.2 MB (69.56%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -2,8 +2,8 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         l, r = 0, len(height)-1
         max_l, max_r = height[l], height[r]
-        res = 0
-
+        res = 0 
+        
         while l < r: 
             if max_l <= max_r: 
                 l += 1
@@ -14,6 +14,4 @@ class Solution:
                 max_r = max(max_r, height[r])
                 res += max_r - height[r]
 
-        return res 
-        
-        
+        return res
