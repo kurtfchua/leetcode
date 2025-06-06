23f15a30b3805016ef857251e24c03941dca8314
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-06 03:27:45 -0500

    Runtime: 373 ms (80.70%), Memory: 19.8 MB (54.70%) - Gitcode

--- 743-network-delay-time/743. Network Delay Time.py
+++ 743-network-delay-time/743. Network Delay Time.py
@@ -3,22 +3,20 @@ class Solution:
         adj_list = defaultdict(list)
 
         for u, v, w in times: 
-            adj_list[u].append((v,w))
-
-        heap = [(0, k)]
+            adj_list[u].append((v, w))
+        
+        min_heap = [(0, k)]
         shortest_paths = {}
-
-        while heap: 
-            w1, u1 = heapq.heappop(heap)
-            if u1 in shortest_paths: 
+        
+        while min_heap: 
+            cost, node = heapq.heappop(min_heap)
+            if node in shortest_paths: 
                 continue
             
-            shortest_paths[u1] = w1
+            shortest_paths[node] = cost
 
-            for u2, w2 in adj_list[u1]:
-                if u2 not in shortest_paths:
-                    heapq.heappush(heap, (w2+w1, u2))
-     
+            for neighbor, cost2 in adj_list[node]:
+                if neighbor not in shortest_paths: 
+                    heapq.heappush(min_heap, (cost+cost2, neighbor))
         
         return max(shortest_paths.values()) if len(shortest_paths) == n else -1
-                     
