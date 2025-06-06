75fe385ef4baa40d3fcbbc6d5b22b60d5c29d8ad
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-06 17:54:09 -0500

    Runtime: 144 ms (14.47%), Memory: 33.4 MB (30.63%) - Gitcode

--- 739-daily-temperatures/739. Daily Temperatures.py
+++ 739-daily-temperatures/739. Daily Temperatures.py
@@ -3,13 +3,10 @@ class Solution:
         res = [0]*len(temperatures)
         stack = []
 
-        for i, v in enumerate(temperatures):
-            while stack and v > stack[-1][0]:
-                prev_temp, prev_i = stack.pop()
-                res[prev_i] = i-prev_i
-            stack.append([v,i])
-        
-        return res
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > stack[-1][0]:
+                    res[stack[-1][1]] = i - stack[-1][1]
+                    stack.pop()
+            stack.append((temperatures[i], i))
 
-        # T O(n), S O(n)
-        
+        return res
