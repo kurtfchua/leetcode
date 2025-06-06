bee70042498a2499a9a49a46e8a3f6eb711dd064
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-06 17:39:54 -0500

    Runtime: 0 ms (100.00%), Memory: 18.2 MB (6.03%) - Gitcode

--- 22-generate-parentheses/22. Generate Parentheses.py
+++ 22-generate-parentheses/22. Generate Parentheses.py
@@ -4,7 +4,7 @@ class Solution:
         combination = []
 
         def dfs(opened, closed):
-            if opened == n and closed == n: 
+            if opened == closed == n: 
                 res.append("".join(combination))
                 return 
             
@@ -13,11 +13,13 @@ class Solution:
                 dfs(opened+1, closed)
                 combination.pop()
             
-            if closed < opened:
+            if closed < opened: 
                 combination.append(")")
                 dfs(opened, closed+1)
                 combination.pop()
 
-        dfs(0, 0)
+        dfs(0,0)
 
-        return res        
+        return res
+
+        
