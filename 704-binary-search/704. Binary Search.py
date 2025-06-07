3aaf14665496982d904967c6e5a965a1ffa6fcb1
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-07 09:43:42 -0500

    Runtime: 0 ms (100.00%), Memory: 18.7 MB (73.92%) - Gitcode

--- 704-binary-search/704. Binary Search.py
+++ 704-binary-search/704. Binary Search.py
@@ -3,13 +3,13 @@ class Solution:
         l, r = 0, len(nums)-1
 
         while l <= r:
-            middle = (l+r)//2
-            if nums[middle] == target:
-                return middle
-            elif nums[middle] < target:
-                l = middle + 1
+            mid = (l+r)//2
+            if nums[mid] == target: 
+                return mid
+            elif nums[mid] < target: 
+                l = mid + 1
             else:
-                r = middle - 1
-        
+                r = mid - 1
+            
         return -1
         
