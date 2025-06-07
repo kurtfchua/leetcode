99891ab10e6bc9f78a0e29b599a8bca97430163e
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-07 09:06:09 -0500

    Runtime: 120 ms (58.86%), Memory: 18 MB (86.50%) - Gitcode

--- 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.py
+++ 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.py
@@ -6,7 +6,7 @@ class Solution:
         count = {}
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            max_f = max(count.values())
+            max_f = max(max_f, count[s[r]])
 
             while (r-l+1) - max_f > k: 
                 count[s[l]] -= 1
