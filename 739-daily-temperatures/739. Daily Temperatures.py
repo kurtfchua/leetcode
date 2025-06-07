1566a5741e46a55d6ab7cafaf38f79ec243f0a33
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-07 00:33:36 -0500

    Runtime: 122 ms (41.59%), Memory: 33.6 MB (24.43%) - Gitcode

--- 739-daily-temperatures/739. Daily Temperatures.py
+++ 739-daily-temperatures/739. Daily Temperatures.py
@@ -5,8 +5,8 @@ class Solution:
 
         for i, v in enumerate(temperatures):
             while stack and v > stack[-1][0]:
-                res[stack[-1][1]] = i - stack[-1][1]
-                stack.pop()
+                _, idx = stack.pop()
+                res[idx] = i-idx
             stack.append((v, i))
         
         return res
