862835c7a57b4755da532d2eb66bafab75eb0b71
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-07 09:58:45 -0500

    Runtime: 0 ms (100.00%), Memory: 18.1 MB (71.88%) - Gitcode

--- 74-search-a-2d-matrix/74. Search a 2D Matrix.py
+++ 74-search-a-2d-matrix/74. Search a 2D Matrix.py
@@ -1,28 +1,27 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows, cols = len(matrix), len(matrix[0])
-        top, bottom = 0, rows-1
-        
-        while top <= bottom:
-            mid = (top+bottom) // 2
-            if matrix[mid][-1] < target:
-                top = mid + 1
-            elif matrix[mid][0] > target:
-                bottom = mid - 1
-            else:
-                break
+        top, bottom = 0, len(matrix)-1
+        row = -1
 
-        if top > bottom:
+        while top <= bottom: 
+            mid = (top+bottom) // 2
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                row = mid
+                break
+            elif target > matrix[mid][-1]:
+                top = mid + 1
+            else:
+                bottom = mid - 1
+        
+        if row == -1: 
             return False
         
-        row_index = (top+bottom) // 2
-        row = matrix[row_index]
-        l, r = 0, cols - 1
-        while l <= r:
-            mid = (l+r) // 2
-            if row[mid] == target:
+        l, r = 0, len(matrix[row])-1
+        while l <= r: 
+            mid = (l+r)//2
+            if matrix[row][mid] == target: 
                 return True
-            elif row[mid] < target:
+            elif matrix[row][mid] < target: 
                 l = mid + 1
             else:
                 r = mid - 1
