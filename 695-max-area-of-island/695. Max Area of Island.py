e0965733117cb9f5016f2dbb34f5269abe5a2917
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-09 06:30:37 -0500

    Runtime: 15 ms (89.02%), Memory: 18.4 MB (41.15%) - Gitcode

--- 695-max-area-of-island/695. Max Area of Island.py
+++ 695-max-area-of-island/695. Max Area of Island.py
@@ -4,10 +4,11 @@ class Solution:
         visited = set()
        
         def dfs(r,c):
-            if r < 0 or r>= rows or c < 0 or c>= cols or (r,c) in visited or grid[r][c] != 1: 
+            if r < 0 or r>= rows or c < 0 or c>= cols or grid[r][c] != 1: 
                 return 0
 
-            visited.add((r,c))
+            #visited.add((r,c))
+            grid[r][c] = 0
             area = 0
             area += 1 + dfs(r-1, c) + dfs(r+1, c) + dfs(r,c-1) + dfs(r, c+1)
 
