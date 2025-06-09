5990c0561e1569f83cf7465db0c912278a2d29eb
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-09 08:14:07 -0500

    Runtime: 44 ms (56.47%), Memory: 25 MB (21.25%) - Gitcode

--- 286-walls-and-gates/286. Walls and Gates.py
+++ 286-walls-and-gates/286. Walls and Gates.py
@@ -4,27 +4,25 @@ class Solution:
         Do not return anything, modify rooms in-place instead.
         """
         rows, cols = len(rooms), len(rooms[0])
-        queue = deque()
         visited = set()
+        queue = deque()
 
-        for r in range(rows):
+        for r in range(rows): 
             for c in range(cols):
                 if rooms[r][c] == 0:
                     queue.append((r,c))
                     visited.add((r,c))
-            
-        distance = 0
+
+        level = 1
         while queue: 
             for i in range(len(queue)):
                 r, c = queue.popleft()
-                rooms[r][c] = distance
-                directions = [(0,1),(1,0),(0,-1),(-1,0)]
+                directions = [(0,1),(1,0),(-1,0),(0,-1)]
                 for dr, dc in directions: 
-                    if (0 <= r+dr < rows and 0 <= c+dc < cols and
-                        rooms[r+dr][c+dc] != -1 and 
-                        (r+dr,c+dc) not in visited):
-                        queue.append((r+dr,c+dc))
-                        visited.add((r+dr,c+dc))
-            distance += 1
-
-
+                    if 0 <= r+dr < rows and 0 <= c+dc < cols and rooms[r+dr][c+dc] == 2**31 - 1 and (r+dr,c+dc) not in visited: 
+                        rooms[r+dr][c+dc] = level
+                        queue.append((r+dr, c+dc))
+                        visited.add((r+dr, c+dc))
+            level += 1
+        
+        return rooms
