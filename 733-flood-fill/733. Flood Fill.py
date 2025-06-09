9987d02fa6ee2b91271f2fe30ede725a64edbefe
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-09 06:12:58 -0500

    Runtime: 3 ms (16.43%), Memory: 18.3 MB (13.54%) - Gitcode

--- 733-flood-fill/733. Flood Fill.py
+++ 733-flood-fill/733. Flood Fill.py
@@ -1,22 +1,22 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         rows, cols = len(image), len(image[0])
-        original_color = image[sr][sc]
         visited = set()
-        def dfs(r,c, ): 
-            if r < 0 or r >= rows or c < 0 or c >= cols or image[r][c] != original_color or (r,c) in visited: 
-                return
+        original_color = image[sr][sc]
+        def dfs(r, c): 
+            if r < 0 or r >= rows or c < 0 or c >= cols or image[r][c] != original_color or (r,c) in visited:
+                return 
 
             visited.add((r,c))
             image[r][c] = color
-            dfs(r-1,c)
-            dfs(r+1,c)
+
+            dfs(r-1, c)
+            dfs(r+1, c)
             dfs(r,c-1)
             dfs(r,c+1)
 
-            return
 
-        dfs(sr, sc)
+        dfs(sr,sc)
 
-        return image            
+        return image 
         
