856aa27e13af3ec1188adcb6d8e63bb0b4c06495
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-09 05:26:55 -0500

    Runtime: 0 ms (100.00%), Memory: 18.3 MB (13.54%) - Gitcode

--- 733-flood-fill/733. Flood Fill.py
+++ 733-flood-fill/733. Flood Fill.py
@@ -3,17 +3,20 @@ class Solution:
         rows, cols = len(image), len(image[0])
         original_color = image[sr][sc]
         visited = set()
-        def dfs(r,c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or (r,c) in visited or image[r][c]!=original_color: 
-                return 
+        def dfs(r,c, ): 
+            if r < 0 or r >= rows or c < 0 or c >= cols or image[r][c] != original_color or (r,c) in visited: 
+                return
+
             visited.add((r,c))
-            
             image[r][c] = color
             dfs(r-1,c)
             dfs(r+1,c)
             dfs(r,c-1)
             dfs(r,c+1)
 
-        dfs(sr,sc)
+            return
 
-        return image        
+        dfs(sr, sc)
+
+        return image            
+        
