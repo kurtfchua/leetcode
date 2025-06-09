e3c629c9aa0de19a66ce38976987c5903dc9ea9e
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-09 06:19:14 -0500

    Runtime: 0 ms (100.00%), Memory: 18.3 MB (13.54%) - Gitcode

--- 733-flood-fill/733. Flood Fill.py
+++ 733-flood-fill/733. Flood Fill.py
@@ -15,8 +15,22 @@ class Solution:
             dfs(r,c-1)
             dfs(r,c+1)
 
+        def bfs(r,c):
+            queue = deque([(r,c)])
+            visited = set([r,c])
+            image[r][c] = color
 
-        dfs(sr,sc)
+            while queue: 
+                r, c = queue.popleft()
+                directions = [(0,1),(1,0),(-1,0),(0,-1)]
+                for dr, dc in directions: 
+                    if 0 <= r+dr < rows and 0 <= c+dc < cols and image[r+dr][c+dc] == original_color and (r+dr,c+dc) not in visited: 
+                        image[r+dr][c+dc] = color
+                        visited.add((r+dr,c+dc))
+                        queue.append((r+dr,c+dc))
+
+        #dfs(sr,sc)
+        bfs(sr,sc)
 
         return image 
         
