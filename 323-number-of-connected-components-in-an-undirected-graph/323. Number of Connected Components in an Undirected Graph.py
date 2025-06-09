017decb5532863e84bda3a939f0f90a3760afc6e
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-09 07:51:19 -0500

    Runtime: 7 ms (52.78%), Memory: 19.8 MB (16.78%) - Gitcode

--- 323-number-of-connected-components-in-an-undirected-graph/323. Number of Connected Components in an Undirected Graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323. Number of Connected Components in an Undirected Graph.py
@@ -1,43 +1,26 @@
-class UnionFind: 
-    def __init__(self, n):
-        self.parents = {}
-        self.ranks = {}
-
-        for i in range(n):
-            self.parents[i] = i 
-            self.ranks[i] = 0 
-    
-    def find(self, n): 
-        p = self.parents[n]
-        while p != self.parents[p]:
-            self.parents[p] = self.parents[self.parents[p]]
-            p = self.parents[p]
-    
-        return p
-    
-    def union(self, n1, n2):
-        p1, p2 = self.find(n1), self.find(n2)
-
-        if p1 == p2: 
-            return False
-        
-        if self.ranks[p1] > self.ranks[p2]:
-            self.parents[p2] = p1
-        elif self.ranks[p1] < self.ranks[p2]:
-            self.parents[p1] = p2
-        else:
-            self.parents[p1] = p2
-            self.ranks[p2] += 1
-        
-        return True
-
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = UnionFind(n)
+        adj_list = defaultdict(list)
 
-        for u, v in edges: 
-            if uf.union(u,v):
-                n-= 1
+        for n1, n2 in edges: 
+            adj_list[n1].append(n2)
+            adj_list[n2].append(n1)
+
+        visited = set()
         
-        return n
+        def dfs(node):
+            if node in visited: 
+                return 
+            
+            visited.add(node)
+
+            for neighbor in adj_list[node]:
+                dfs(neighbor)
+
+        count = 0
+        for i in range(n):
+            if i not in visited: 
+                dfs(i)
+                count += 1
         
+        return count
