1a2a9eaad366f905c9b11d5225c04a78e9b871cd
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-12 17:36:36 -0500

    Runtime: 216 ms (5.00%), Memory: 14.6 MB (100.00%) - Gitcode

---
+++ 387-first-unique-character-in-a-string/387. First Unique Character in a String.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        map = {}
+        for i in range(len(s)):
+            if s[i] not in map: 
+                map[s[i]] = (i, 1)
+            else:
+                map[s[i]] = (map[s[i]][0],map[s[i]][1]+1)
+        
+        for l in map: 
+            if map[l][1] == 1: 
+                return map[l][0]
+        return -1
+        
