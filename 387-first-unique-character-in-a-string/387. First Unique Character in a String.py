9e6b4b153083b6bb4cbbd343d5e34ed16123c209
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-12 17:36:42 -0500

    Runtime: 228 ms (5.00%), Memory: 14.5 MB (100.00%) - Gitcode

--- 387-first-unique-character-in-a-string/387. First Unique Character in a String.py
+++ 387-first-unique-character-in-a-string/387. First Unique Character in a String.py
@@ -7,6 +7,8 @@ class Solution:
             else:
                 map[s[i]] = (map[s[i]][0],map[s[i]][1]+1)
         
+        
+    
         for l in map: 
             if map[l][1] == 1: 
                 return map[l][0]
