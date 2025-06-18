198120d91d26adf4452c63fcfcbd0c5c88b33171
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-18 10:38:33 -0500

    Runtime: 12 ms (8.47%), Memory: 18.9 MB (62.96%) - Gitcode

--- 110-balanced-binary-tree/110. Balanced Binary Tree.py
+++ 110-balanced-binary-tree/110. Balanced Binary Tree.py
@@ -6,13 +6,14 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node):
-            if not node: return [True, 0]
 
-            left, right = dfs(node.left), dfs(node.right)
-            balanced = (left[0] and right[0] and abs(left[1]-right[1]) <= 1)
+        def dfs(root):
+            if not root: return [True, 0]
 
-            return [balanced, 1 + max(left[1], right[1])]
+            left, right = dfs(root.left), dfs(root.right)
+
+            balanced = abs(left[1]-right[1]) <= 1        
+
+            return balanced and left[0] and right[0], 1 + max(left[1], right[1])
         
         return dfs(root)[0]
-        
