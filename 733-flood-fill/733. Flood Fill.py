a9995692d631d796e9237ae25b2040fb6c77a92b
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-18 10:05:12 -0500

    Runtime: 4 ms (6.14%), Memory: 18.3 MB (13.78%) - Gitcode

--- 733-flood-fill/733. Flood Fill.py
+++ 733-flood-fill/733. Flood Fill.py
@@ -1,36 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         rows, cols = len(image), len(image[0])
+        starting_color = image[sr][sc]
         visited = set()
-        original_color = image[sr][sc]
-        def dfs(r, c): 
-            if r < 0 or r >= rows or c < 0 or c >= cols or image[r][c] != original_color or (r,c) in visited:
-                return 
 
-            visited.add((r,c))
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or (r,c) in visited or image[r][c] != starting_color: 
+                return 
+            
             image[r][c] = color
+            visited.add((r,c))
 
             dfs(r-1, c)
-            dfs(r+1, c)
             dfs(r,c-1)
+            dfs(r+1,c)
             dfs(r,c+1)
+        
+        dfs(sr, sc)
 
-        def bfs(r,c):
-            queue = deque([(r,c)])
-            visited = set([r,c])
-            image[r][c] = color
+        return image
 
-            while queue: 
-                r, c = queue.popleft()
-                directions = [(0,1),(1,0),(-1,0),(0,-1)]
-                for dr, dc in directions: 
-                    if 0 <= r+dr < rows and 0 <= c+dc < cols and image[r+dr][c+dc] == original_color and (r+dr,c+dc) not in visited: 
-                        image[r+dr][c+dc] = color
-                        visited.add((r+dr,c+dc))
-                        queue.append((r+dr,c+dc))
 
-        #dfs(sr,sc)
-        bfs(sr,sc)
-
-        return image 
         
