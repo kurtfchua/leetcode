56034d6b63ce0e8067e61d4165861e26bbd4268a
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-17 22:50:29 -0500

    Runtime: 0 ms (100.00%), Memory: 18.7 MB (73.92%) - Gitcode

--- 704-binary-search/704. Binary Search.py
+++ 704-binary-search/704. Binary Search.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums)-1
 
-        while l <= r:
+        while l <= r: 
             mid = (l+r)//2
             if nums[mid] == target: 
                 return mid
@@ -10,6 +10,6 @@ class Solution:
                 l = mid + 1
             else:
                 r = mid - 1
-            
+        
         return -1
         
