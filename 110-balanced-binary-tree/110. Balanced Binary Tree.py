d354f0019be6e44a4c2a9508446d307bb830ad62
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-18 11:05:20 -0500

    Runtime: 7 ms (30.85%), Memory: 18.9 MB (62.96%) - Gitcode

--- 110-balanced-binary-tree/110. Balanced Binary Tree.py
+++ 110-balanced-binary-tree/110. Balanced Binary Tree.py
@@ -12,8 +12,8 @@ class Solution:
 
             left, right = dfs(root.left), dfs(root.right)
 
-            balanced = abs(left[1]-right[1]) <= 1        
+            balanced = abs(left[1]-right[1]) <= 1 and left[0] and right[0]      
 
-            return balanced and left[0] and right[0], 1 + max(left[1], right[1])
+            return balanced, 1 + max(left[1], right[1])
         
         return dfs(root)[0]
