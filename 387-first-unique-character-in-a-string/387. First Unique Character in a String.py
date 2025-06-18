91bd803afb88bcb4b239cf2ebccf81b049daf52c
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-17 22:59:24 -0500

    Runtime: 51 ms (80.81%), Memory: 18.3 MB (15.89%) - Gitcode

--- 387-first-unique-character-in-a-string/387. First Unique Character in a String.py
+++ 387-first-unique-character-in-a-string/387. First Unique Character in a String.py
@@ -1,18 +1,9 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        hashmap = {}
+        freq = Counter(s)
 
-        for i in range(len(s)):
-            if s[i] in hashmap:
-                hashmap[s[i]][0] += 1
-            else:
-                hashmap[s[i]] = [1, i] 
-        
-        min_index = float('inf')
+        for i in range(len(s)): 
+            if freq[s[i]] == 1:
+                return i 
 
-        for k, v in hashmap.items():
-            if v[0] == 1:
-                min_index = min(min_index, v[1])
-        
-        return min_index if min_index != float('inf') else -1
-        
+        return -1 
