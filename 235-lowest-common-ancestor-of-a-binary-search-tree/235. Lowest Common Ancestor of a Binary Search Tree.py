03956e973909433176ddcf00671016a4f0826b46
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-18 10:22:45 -0500

    Runtime: 58 ms (59.31%), Memory: 21 MB (47.32%) - Gitcode

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -7,13 +7,16 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # bst left child is less and right child is more than its parent
+        # have lca as root by default 
+        # if both values are less than the lca then their ancestor exists in the left subtree
+        # if both values are greater than the lca then their ancestor exists in the right subtree
+        
         lca = root
-
-        while lca:
-            if p.val < lca.val and q.val < lca.val:
+        while lca: 
+            if p.val < lca.val and q.val < lca.val: 
                 lca = lca.left
-            elif p.val > lca.val and q.val > lca.val:
+            elif p.val > lca. val and q.val > lca.val:
                 lca = lca.right
             else:
                 return lca
-        
