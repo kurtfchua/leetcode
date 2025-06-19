9f1b8fb137fe9ffece3d828587af8af9ffebe2f6
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-19 12:47:07 -0500

    Runtime: 25 ms (30.55%), Memory: 17.9 MB (54.25%) - Gitcode

--- 383-ransom-note/383. Ransom Note.py
+++ 383-ransom-note/383. Ransom Note.py
@@ -1,25 +1,14 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransom_dict, magazine_dict = {} , {}
+        counts = [0]*26
 
+        for c in magazine: 
+            counts[ord(c)-ord('a')] += 1
+        
         for c in ransomNote:
-            if c not in ransom_dict:
-                ransom_dict[c] = 1
-            else:
-                ransom_dict[c] = ransom_dict[c] + 1
-        
-        for c in magazine:
-            if c not in magazine_dict:
-                magazine_dict[c] = 1
-            else:
-                magazine_dict[c] = magazine_dict[c] + 1
-        
-        for key in ransom_dict:
-            if key not in magazine_dict:
-                return False
-            if ransom_dict[key] > magazine_dict[key]:
+            counts[ord(c)-ord('a')] -= 1
+            if counts[ord(c)-ord('a')] < 0: 
                 return False
         
         return True
-
         
