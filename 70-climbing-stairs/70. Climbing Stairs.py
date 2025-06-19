277cc1a9285683ad9a5bf6525971c738b678bb23
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-19 12:53:55 -0500

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (48.37%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -1,36 +1,21 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        def basic(n):
+        def recur(n): 
             if n <= 2: 
                 return n 
             
-            return basic(n-1) + basic(n-2)
+            return recur(n-1) + recur(n-2)
         
         mem = {}
-        def top_bottom(n):
-            if n <= 2:
-                return n
-            if n in mem:
+        def top_down(n):
+            if n in mem: 
                 return mem[n]
-            
-            mem[n] = top_bottom(n-1) + top_bottom(n-2)
-
-            return mem[n]
-        
-        def bottom_up(n):
-            if n <= 2:
+            if n <= 2: 
                 return n 
             
-            a, b = 1, 2
-            i = 3
-            while i <= n:
-                a, b = b, a+b
-                i += 1
+            mem[n] = top_down(n-1) + top_down(n-2)
+            return mem[n]
 
-            return b 
-
-        #return basic(n)
-        #return top_bottom(n)
-        return bottom_up(n)
-        
+        #return recur(n)
+        return top_down(n)
