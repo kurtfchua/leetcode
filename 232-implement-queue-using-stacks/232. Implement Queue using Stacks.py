1b03004723714c09145f0a789c11df92a9ababc0
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-06-19 12:24:56 -0500

    Runtime: 0 ms (100.00%), Memory: 18 MB (19.58%) - Gitcode

--- 232-implement-queue-using-stacks/232. Implement Queue using Stacks.py
+++ 232-implement-queue-using-stacks/232. Implement Queue using Stacks.py
@@ -4,25 +4,25 @@ class MyQueue:
         self.inbox = []
         self.outbox = []
         
-
     def push(self, x: int) -> None:
         self.inbox.append(x)
-        
+
     def pop(self) -> int:
         if not self.outbox:
             while self.inbox:
                 self.outbox.append(self.inbox.pop())
+        
         return self.outbox.pop()
-
+        
     def peek(self) -> int:
         if not self.outbox:
             while self.inbox:
                 self.outbox.append(self.inbox.pop())
-        return self.outbox[-1]        
+        
+        return self.outbox[-1]
 
     def empty(self) -> bool:
-        return not self.outbox and not self.inbox
-        
+        return not self.inbox and not self.outbox
 
 
 # Your MyQueue object will be instantiated and called as such:
