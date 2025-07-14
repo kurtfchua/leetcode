62e8210e73d6465e486e70a437624e5cacad186c
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-07-14 11:38:06 -0500

    Runtime: 8 ms (86.80%), Memory: 31.5 MB (53.37%) - Gitcode

--- 217-contains-duplicate/217. Contains Duplicate.py
+++ 217-contains-duplicate/217. Contains Duplicate.py
@@ -2,10 +2,10 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         seen = set()
 
-        for num in nums: 
-            if num in seen: 
+        for num in nums:
+            if num in seen:
                 return True
             seen.add(num)
-        
+    
         return False
         
