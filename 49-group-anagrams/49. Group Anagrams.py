8c49c244e5f5cc1b97deabd538955bed0e4dc4d1
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-08-01 22:17:54 -0500

    Runtime: 20 ms (25.44%), Memory: 22.7 MB (12.49%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.py
+++ 49-group-anagrams/49. Group Anagrams.py
@@ -1,11 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams_map = defaultdict(list)
+        hashmap = defaultdict(list)
 
         for word in strs:
-            char_count = [0]*26
+            counts = [0]*26
             for c in word:
-                char_count[ord(c)-ord('a')] += 1
-            anagrams_map[tuple(char_count)].append(word)
-
-        return list(anagrams_map.values())        
+                counts[ord(c)-ord('a')] += 1
+            hashmap[tuple(counts)].append(word)
+        
+        return list(hashmap.values())
+        
