b7884a989ee50772ff72fbecd74371c15279e70c
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-08-02 16:29:48 -0500

    Runtime: 3 ms (90.60%), Memory: 21.3 MB (61.95%) - Gitcode

--- 347-top-k-frequent-elements/347. Top K Frequent Elements.py
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -1,18 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # get the counts in nums for each num in a counter hashmap
-        # heapify a list with the counts as well as the num 
         counts_map = Counter(nums)
-        heap = []
+        heap = [(count, num) for num, count in counts_map.items()]
+        heapq.heapify(heap)
 
-        for num, v in counts_map.items():
-            heapq.heappush(heap, (v, num))
+        while len(heap) > k:
+            heapq.heappop(heap)
 
-            if len(heap) > k:
-                heapq.heappop(heap)
-        
         res = []
-        for v, num in heap:
+        for count, num in heap:
             res.append(num)
-
+        
         return res
