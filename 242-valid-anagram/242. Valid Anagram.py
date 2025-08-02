59075c88c604ec11caa4c21c8d0f9e93768f8be3
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-08-01 21:38:14 -0500

    Runtime: 11 ms (72.08%), Memory: 17.8 MB (86.49%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -2,14 +2,14 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
 
-        counts = [0]*26
-
-        for i in range(len(s)):
-            counts[ord(s[i])-ord('a')] += 1 
-            counts[ord(t[i])-ord('a')] -= 1 
+        hashmap = defaultdict(int)
         
-        for c in counts: 
-            if c != 0: 
+        for i in range(len(s)):
+            hashmap[s[i]] += 1
+            hashmap[t[i]] -= 1
+        
+        for c, count in hashmap.items():
+            if count != 0:
                 return False
         
         return True
