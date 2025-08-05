0fa7dafe8a63afecc3ae7c08e3fd35a75ecb3bbb
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-08-05 00:10:50 -0500

    Runtime: 17 ms (92.40%), Memory: 23.4 MB (53.62%) - Gitcode

--- 238-product-of-array-except-self/238. Product of Array Except Self.py
+++ 238-product-of-array-except-self/238. Product of Array Except Self.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
-        
+
         suffix = 1
         for i in range(len(nums)-1,-1,-1):
             res[i] *= suffix
