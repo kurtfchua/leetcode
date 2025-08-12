db139498f246f3e814ce6333e9aa73201f40b7c4
kurt <94031312+kurtfchua@users.noreply.github.com>
2025-08-11 22:14:01 -0500

    Runtime: 886 ms (18.54%), Memory: 20.7 MB (36.17%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -2,26 +2,28 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
-        i = 0
 
         for i in range(len(nums)):
             if i > 0 and nums[i-1] == nums[i]:
                 continue
-            l, r = i + 1, len(nums)-1
+            
+            j, k = i+1, len(nums) - 1
 
-            while l < r:
-                if nums[l] + nums[r] == -nums[i]:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1  
-                    r -= 1
-                    while l < r and nums[r] == nums[r+1]:
-                        r -= 1
-                elif nums[l] + nums[r] < -nums[i]:
-                    l += 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+
+                    while j < len(nums) - 1 and nums[j] == nums[j-1]:
+                        j += 1
+                    
+                    while k > j and nums[k] == nums[k+1]:
+                        k -= 1
+
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
                 else:
-                    r -= 1
+                    k -= 1
         
         return res
-        
